c057495a67be377d074c2a53fe6cca839305a5f0
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2014-09-09 17:29:39 +0200

    Upgrade content preview

--- portal-ui/src/main/java/org/entando/entando/plugins/jacms/apsadmin/content/preview/ContentPreviewDispenser.java
+++ portal-ui/src/main/java/org/entando/entando/plugins/jacms/apsadmin/content/preview/ContentPreviewDispenser.java
@@ -17,15 +17,24 @@
 */
 package org.entando.entando.plugins.jacms.apsadmin.content.preview;
 
+import com.agiletec.aps.system.RequestContext;
+import com.agiletec.aps.system.common.entity.model.attribute.AttributeRole;
+import com.agiletec.aps.system.services.group.Group;
+import com.agiletec.aps.system.services.user.UserDetails;
+
+import com.agiletec.plugins.jacms.aps.system.services.content.helper.PublicContentAuthorizationInfo;
+import com.agiletec.plugins.jacms.aps.system.services.content.model.Content;
+import com.agiletec.plugins.jacms.aps.system.services.dispenser.BaseContentDispenser;
+import com.agiletec.plugins.jacms.aps.system.services.dispenser.ContentRenderizationInfo;
+import com.agiletec.plugins.jacms.apsadmin.content.ContentActionConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.agiletec.aps.system.RequestContext;
-import com.agiletec.plugins.jacms.aps.system.services.content.model.Content;
-import com.agiletec.plugins.jacms.aps.system.services.dispenser.BaseContentDispenser;
-import com.agiletec.plugins.jacms.apsadmin.content.ContentActionConstants;
 
 /**
  * Fornisce i contenuti formattati per la funzione preview da redazione contenuti.
@@ -37,33 +46,35 @@ public class ContentPreviewDispenser extends BaseContentDispenser {
 	private static final Logger _logger = LoggerFactory.getLogger(ContentPreviewDispenser.class);
 	
 	@Override
-	public String getRenderedContent(String contentId, long modelId, String langCode, RequestContext reqCtx) {
+	public ContentRenderizationInfo getRenderizationInfo(String contentId, long modelId, String langCode, RequestContext reqCtx) {
+		return super.getRenderizationInfo(contentId, modelId, langCode, reqCtx);
+	}
+	
+	@Override
+	public ContentRenderizationInfo getBaseRenderizationInfo(PublicContentAuthorizationInfo authInfo, 
+			String contentId, long modelId, String langCode, UserDetails currentUser, RequestContext reqCtx) {
 		HttpServletRequest request = reqCtx.getRequest();
 		String contentOnSessionMarker = (String) request.getAttribute("contentOnSessionMarker");
 		if (null == contentOnSessionMarker || contentOnSessionMarker.trim().length() == 0) {
 			contentOnSessionMarker = request.getParameter("contentOnSessionMarker");
 		}
-		Content contentOnSession = (Content) request.getSession()
-				.getAttribute(ContentActionConstants.SESSION_PARAM_NAME_CURRENT_CONTENT_PREXIX + contentOnSessionMarker);
-		String renderedContent = this.getRenderedContent(contentOnSession, modelId, langCode, reqCtx);
-		return renderedContent;
-	}
-	
-	protected String getRenderedContent(Content content, long modelId, String langCode, RequestContext reqCtx) {
-		String renderedContent = null;
-		boolean ok = false;
+		ContentRenderizationInfo renderInfo = null;
 		try {
-			renderedContent = this.getContentRender().render(content, modelId, langCode, reqCtx);
-			ok = true;
+			List<Group> userGroups = (null != currentUser) ? this.getAuthorizationManager().getUserGroups(currentUser) : new ArrayList<Group>();
+			if (authInfo.isUserAllowed(userGroups)) {
+				Content contentOnSession = (Content) request.getSession()
+						.getAttribute(ContentActionConstants.SESSION_PARAM_NAME_CURRENT_CONTENT_PREXIX + contentOnSessionMarker);
+				String renderedContent = this.buildRenderedContent(contentOnSession, modelId, langCode, reqCtx);
+				if (null != renderedContent && renderedContent.trim().length() > 0) {
+					List<AttributeRole> roles = this.getContentManager().getAttributeRoles();
+					renderInfo = new ContentRenderizationInfo(contentOnSession, renderedContent, modelId, langCode, roles);
+				}
+			}
 		} catch (Throwable t) {
-			_logger.error("Error in getRenderedContent",  t);
+			_logger.error("Error while rendering content {}", contentId, t);
+			return null;
 		}
-		if (!ok) {
-			_logger.warn("Preview problem for {}", content.getId());
-			return "";
-		}
-		renderedContent = this.getLinkResolverManager().resolveLinks(renderedContent, reqCtx);
-		return renderedContent;
+		return renderInfo;
 	}
 	
 }

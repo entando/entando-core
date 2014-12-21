0a52bfddb9f8e428f7606d8cabff8a8e7414c7fb
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2014-12-21 16:42:53 +0100

    entando-admin-console: license upgrade

--- admin-console/src/main/java/com/agiletec/apsadmin/system/ChainingInterceptor.java
+++ admin-console/src/main/java/com/agiletec/apsadmin/system/ChainingInterceptor.java
@@ -1,17 +1,15 @@
 /*
- * Copyright 2002-2007,2009 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2013-Present Entando Corporation (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 package com.agiletec.apsadmin.system;
 

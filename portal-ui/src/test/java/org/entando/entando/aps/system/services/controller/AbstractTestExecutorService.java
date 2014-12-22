0989fa986cb3afa709900bfa87eb08b8c9063d8d
eugeniosant <eugenio.santoboni@gmail.com>
2014-12-22 14:18:09 +0100

    entando-portal-ui: license upgrade

--- portal-ui/src/test/java/org/entando/entando/aps/system/services/controller/AbstractTestExecutorService.java
+++ portal-ui/src/test/java/org/entando/entando/aps/system/services/controller/AbstractTestExecutorService.java
@@ -1,20 +1,16 @@
 /*
-*
-* Copyright 2014 Entando S.r.l. (http://www.entando.com) All rights reserved.
-*
-* This file is part of Entando software.
-* Entando is a free software;
-* You can redistribute it and/or modify it
-* under the terms of the GNU General Public License (GPL) as published by the Free Software Foundation; version 2.
-* 
-* See the file License for the specific language governing permissions   
-* and limitations under the License
-* 
-* 
-* 
-* Copyright 2014 Entando S.r.l. (http://www.entando.com) All rights reserved.
-*
-*/
+ * Copyright 2013-Present Entando Corporation (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package org.entando.entando.aps.system.services.controller;
 
 import com.agiletec.aps.BaseTestCase;

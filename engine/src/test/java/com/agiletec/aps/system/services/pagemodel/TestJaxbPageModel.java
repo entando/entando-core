8b91024b38d84be6ed701011c9352c401b2735b5
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2015-09-25 13:07:11 +0200

    Upgrade Copyright

--- engine/src/test/java/com/agiletec/aps/system/services/pagemodel/TestJaxbPageModel.java
+++ engine/src/test/java/com/agiletec/aps/system/services/pagemodel/TestJaxbPageModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-Present Entando Corporation (http://www.entando.com) All rights reserved.
+ * Copyright 2013-Present Entando Inc. (http://www.entando.com) All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free

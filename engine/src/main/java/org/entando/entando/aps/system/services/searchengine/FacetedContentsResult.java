05f0aa693671773462b3ca5f41757caf91554761
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2015-09-25 13:26:27 +0200

    Upgrade Copyright

--- engine/src/main/java/org/entando/entando/aps/system/services/searchengine/FacetedContentsResult.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/searchengine/FacetedContentsResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ * Copyright 2015-Present Entando Inc. (http://www.entando.com) All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

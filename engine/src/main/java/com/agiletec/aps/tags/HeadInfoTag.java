b8823e2e05cd1f8d3e47c6c9669377f38324a724
eugeniosant <eugenio.santoboni@gmail.com>
2015-11-17 12:16:32 +0100

    add freemarker handling on HeadInfoTag

--- engine/src/main/java/com/agiletec/aps/tags/HeadInfoTag.java
+++ engine/src/main/java/com/agiletec/aps/tags/HeadInfoTag.java
@@ -13,16 +13,19 @@
  */
 package com.agiletec.aps.tags;
 
+import com.agiletec.aps.system.RequestContext;
+import com.agiletec.aps.system.SystemConstants;
+import com.agiletec.aps.tags.util.HeadInfoContainer;
+
+import freemarker.core.Environment;
+import freemarker.ext.beans.StringModel;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.agiletec.aps.system.RequestContext;
-import com.agiletec.aps.system.SystemConstants;
-import com.agiletec.aps.tags.util.HeadInfoContainer;
 
 /**
  * Tag for the declaration of the informations to insert in the header of the HTML page
@@ -31,6 +34,7 @@ public class HeadInfoTag extends TagSupport {
 
 	private static final Logger _logger = LoggerFactory.getLogger(HeadInfoTag.class);
 	
+	@Override
 	public int doEndTag() throws JspException {
 		ServletRequest request =  this.pageContext.getRequest();
 		RequestContext reqCtx = (RequestContext) request.getAttribute(RequestContext.REQCTX);
@@ -41,50 +45,77 @@ public class HeadInfoTag extends TagSupport {
 				if(_info != null) {
 					info = _info;
 				} else {
-					info = this.pageContext.getAttribute(_var);
+					info = this.extractAttribute();
 				}
 				headInfo.addInfo(_type, info);
 			}
 		} catch (Throwable t) {
 			_logger.error("Error closing tag", t);
-			//ApsSystemUtils.logThrowable(t, this, "doEndTag");
 			throw new JspException("Error closing tag ", t);
 		}
-		return EVAL_BODY_INCLUDE;
+		return super.doEndTag();
 	}
-
+	
+    private String extractAttribute() throws JspException {
+		if (null == this.getVar()) {
+			return null;
+		}
+		String objectToString = null;
+		try {
+			Object object = this.pageContext.getAttribute(this.getVar());
+			if (null == object) {
+				Environment environment = Environment.getCurrentEnvironment();
+				if (null != environment) {
+					Object wrapper = environment.getVariable(this.getVar());
+					if (null != wrapper) {
+						if (wrapper instanceof StringModel) {
+							object = ((StringModel) wrapper).getWrappedObject();
+						} else {
+							object = wrapper;
+						}
+					}
+				}
+			}
+			if (null != object) {
+				objectToString = object.toString();
+			}
+        } catch (Throwable t) {
+        	_logger.error("error extracting freemarker attribute", t);
+            throw new JspException("Error extracting freemarker attribute", t);
+        }
+		return objectToString;
+    }
+	
+	@Override
 	public void release() {
 		_type = null;
 		_var = null;
 		_info = null;
 	}
-
+	
 	public String getInfo() {
 		return _info;
 	}
-
 	public void setInfo(String info) {
 		this._info = info;
 	}
-
+	
 	public void setVar(String var) {
 		this._var = var;
 	}
-
 	public String getVar() {
 		return _var;
 	}
-
+	
 	public String getType() {
 		return _type;
 	}
-
 	public void setType(String type) {
 		this._type = type;
 	}
-
+	
 	private String _var;
 	private String _info;
 	private String _type;
-
+	
 }

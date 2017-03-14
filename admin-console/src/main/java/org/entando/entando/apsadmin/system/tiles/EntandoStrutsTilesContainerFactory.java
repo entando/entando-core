7566b5d5c78238e40d3376d629ff5b9933196c09
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2017-03-14 20:25:50 +0100

    Improve Tiles integration

--- admin-console/src/main/java/org/entando/entando/apsadmin/system/tiles/EntandoStrutsTilesContainerFactory.java
+++ admin-console/src/main/java/org/entando/entando/apsadmin/system/tiles/EntandoStrutsTilesContainerFactory.java
@@ -11,38 +11,52 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package org.entando.entando.apsadmin.system.tiles.impl;
+package org.entando.entando.apsadmin.system.tiles;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.ServletContext;
-import org.apache.tiles.impl.BasicTilesContainer;
+import org.apache.struts2.tiles.StrutsTilesContainerFactory;
+import org.apache.tiles.definition.DefinitionsFactory;
 
 /**
  * @see org.entando.entando.apsadmin.system.tiles.EntandoStrutsTilesListener
- * @author zuanni G.Cocco
+ * @author zuanni G.Cocco - E.Santoboni
  */
-public class EntandoBasicTilesContainer extends BasicTilesContainer {
+public class EntandoStrutsTilesContainerFactory extends StrutsTilesContainerFactory {
 	
-	/*
+	private ServletContext _servletContext;
+	
+	protected EntandoStrutsTilesContainerFactory(ServletContext servletContext) {
+		this.setServletContext(servletContext);
+	}
+	
 	@Override
-	public List<String> getResourceNames(String resourceString) {
+	protected Set<String> getTilesDefinitions(Map<String, String> params) {
+        if (params.containsKey(DefinitionsFactory.DEFINITIONS_CONFIG)) {
+            return this.getResourceNames(params.get(DefinitionsFactory.DEFINITIONS_CONFIG));
+        }
+        return this.getResourceNames(TILES_DEFAULT_PATTERN);
+    }
+	
+	//@Override
+	protected Set<String> getResourceNames(String resourceString) {
 		StringTokenizer tokenizer = new StringTokenizer(resourceString, ",");
-		List<String> filenames = new ArrayList<String>();
-		System.out.println(EntandoBasicTilesContainer.class.getName() + " Tiles Conf files: ");
+		Set<String> filenames = new HashSet<String>();
+		System.out.println(EntandoStrutsTilesContainerFactory.class.getName() + " Tiles Conf files: ");
 		while (tokenizer.hasMoreTokens()) {
 			String currentFilename = tokenizer.nextToken().trim();
 			int index = currentFilename.indexOf(AXTER);
 			if (-1 == index) {
 				filenames.add(currentFilename);
-				System.out.println(EntandoBasicTilesContainer.class.getName() + " " + currentFilename);
+				System.out.println(EntandoStrutsTilesContainerFactory.class.getName() + " " + currentFilename);
 			} else {
-				List<String> confFiles = new ArrayList<String>();
+				Set<String> confFiles = new HashSet<String>();
 				String rootInspectionDir = currentFilename.substring(0, index);
 				this.inspectResources(currentFilename, rootInspectionDir, confFiles);
 				filenames.addAll(confFiles);
@@ -50,8 +64,8 @@ public class EntandoBasicTilesContainer extends BasicTilesContainer {
 		}
 		return filenames;
 	}
-	*/
-	private void inspectResources(String currentFilenamesConf, String rootInspectionDir, List<String> confFiles) {
+	
+	private void inspectResources(String currentFilenamesConf, String rootInspectionDir, Set<String> confFiles) {
 		Set<String> resourcesPath = this.getServletContext().getResourcePaths(rootInspectionDir);
 		if (null != resourcesPath) {
 			Iterator<String> it = resourcesPath.iterator();
@@ -59,7 +73,7 @@ public class EntandoBasicTilesContainer extends BasicTilesContainer {
 				String current = it.next();
 				if (!current.endsWith("/") && this.isConfResource(current, currentFilenamesConf)) {
 					confFiles.add(current);
-					System.out.println(EntandoBasicTilesContainer.class.getName() + " " + current);
+					System.out.println(EntandoStrutsTilesContainerFactory.class.getName() + " " + current);
 				} else {
 					this.inspectResources(currentFilenamesConf, current, confFiles);
 				}
@@ -80,8 +94,6 @@ public class EntandoBasicTilesContainer extends BasicTilesContainer {
 	public ServletContext getServletContext() {
 		return _servletContext;
 	}
-	
-	private ServletContext _servletContext;
 	
 	private final static String REG_EXP = "[\\\\w,\\\\-,_]*";
 	private final static String AXTER = "**";

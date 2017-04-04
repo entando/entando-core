c94cb11741dffae23a161b08311d1f12556b6177
Pasquale <p.addeo@entando.com>
2017-04-04 12:24:11 +0200

    user configuration

---
+++ admin-console/src/main/java/org/entando/entando/apsadmin/user/UserSettingsAction.java
@@ -0,0 +1,34 @@
+package org.entando.entando.apsadmin.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.agiletec.aps.system.SystemConstants;
+import com.agiletec.apsadmin.admin.BaseAdminAction;
+import com.agiletec.apsadmin.admin.SystemParamsUtils;
+
+public class UserSettingsAction extends BaseAdminAction {
+
+	private static final Logger _logger = LoggerFactory.getLogger(UserSettingsAction.class);
+	
+    /**
+	 * Update the system params.
+	 * @return the result code.
+	 */
+	public String updateSystemParams() {
+        try {
+            this.initLocalMap();
+            this.updateLocalParams(true);
+            String xmlParams = this.getConfigManager().getConfigItem(SystemConstants.CONFIG_ITEM_PARAMS);
+            this.extractExtraParameters();
+            String newXmlParams = SystemParamsUtils.getNewXmlParams(xmlParams, this.getSystemParams());
+            this.getConfigManager().updateConfigItem(SystemConstants.CONFIG_ITEM_PARAMS, newXmlParams);
+            this.addActionMessage(this.getText("message.configSystemParams.ok"));
+        } catch (Throwable t) {
+        	_logger.error("error in updateSystemParams", t);
+            return FAILURE;
+        }
+        return SUCCESS;
+    }
+	
+}

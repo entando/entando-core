e83098a4fd2376633a9776c01202bf56c63879ff
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2017-05-18 12:33:09 +0200

    ENQT-162: Fix Bug on category settings (#113)
    * Upgrade actions to config system parameters
    
    * added EOF newline

--- admin-console/src/main/java/com/agiletec/apsadmin/portal/PageSettingsAction.java
+++ admin-console/src/main/java/com/agiletec/apsadmin/portal/PageSettingsAction.java
@@ -13,35 +13,13 @@
  */
 package com.agiletec.apsadmin.portal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.agiletec.aps.system.SystemConstants;
-import com.agiletec.aps.system.services.baseconfig.SystemParamsUtils;
 import com.agiletec.apsadmin.admin.BaseAdminAction;
 
 public class PageSettingsAction extends BaseAdminAction {
 
-	private static final Logger _logger = LoggerFactory.getLogger(PageSettingsAction.class);
-	
-    /**
-	 * Update the system params.
-	 * @return the result code.
-	 */
+	@Override
 	public String updateSystemParams() {
-        try {
-            this.initLocalMap();
-            this.updateLocalParams(true);
-            String xmlParams = this.getConfigManager().getConfigItem(SystemConstants.CONFIG_ITEM_PARAMS);
-            this.extractExtraParameters();
-            String newXmlParams = SystemParamsUtils.getNewXmlParams(xmlParams, this.getSystemParams());
-            this.getConfigManager().updateConfigItem(SystemConstants.CONFIG_ITEM_PARAMS, newXmlParams);
-            this.addActionMessage(this.getText("message.configSystemParams.ok"));
-        } catch (Throwable t) {
-        	_logger.error("error in updateSystemParams", t);
-            return FAILURE;
-        }
-        return SUCCESS;
-    }
-	
+		return this.updateSystemParams(true);
+	}
+
 }

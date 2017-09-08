473c2b43340e49bc306e38a002978228af94672a
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2017-09-08 12:17:36 +0200

    Data type - test refactoring

--- admin-console/src/test/java/com/agiletec/plugins/jacms/apsadmin/category/TestTrashReferencedCategory.java
+++ admin-console/src/test/java/com/agiletec/plugins/jacms/apsadmin/category/TestTrashReferencedCategory.java
@@ -27,12 +27,13 @@ import com.agiletec.plugins.jacms.aps.system.JacmsSystemConstants;
  * @author E.Santoboni
  */
 public class TestTrashReferencedCategory extends ApsAdminBaseTestCase {
-	
+
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		this.init();
 	}
-	
+
 	public void testTrashReferencedCategory() throws Throwable {
 		String categoryCode = "evento";
 		Category masterCategory = this._categoryManager.getCategory(categoryCode);
@@ -43,14 +44,14 @@ public class TestTrashReferencedCategory extends ApsAdminBaseTestCase {
 			this.addParameter("selectedNode", categoryCode);
 			String result = this.executeAction();
 			assertEquals("references", result);
-			
+
 			CategoryAction action = (CategoryAction) this.getAction();
 			Map<String, List> references = action.getReferences();
-			assertEquals(1, references.size());
-			
-			List contentReferences = references.get(JacmsSystemConstants.CONTENT_MANAGER+"Utilizers");
+			assertEquals(2, references.size());
+
+			List contentReferences = references.get(JacmsSystemConstants.CONTENT_MANAGER + "Utilizers");
 			assertEquals(2, contentReferences.size());
-			for (int i=0; i<contentReferences.size(); i++) {
+			for (int i = 0; i < contentReferences.size(); i++) {
 				String contentId = (String) contentReferences.get(i);
 				assertTrue(contentId.equals("EVN193") || contentId.equals("EVN192"));
 			}
@@ -58,11 +59,11 @@ public class TestTrashReferencedCategory extends ApsAdminBaseTestCase {
 			throw t;
 		}
 	}
-	
+
 	private void init() throws Exception {
 		this._categoryManager = (ICategoryManager) this.getService(SystemConstants.CATEGORY_MANAGER);
 	}
-	
+
 	private ICategoryManager _categoryManager;
 
 }

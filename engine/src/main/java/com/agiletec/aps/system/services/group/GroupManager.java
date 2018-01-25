3d1af06760b6572aab3e1d2ef443d0faa9161eb3
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-01-25 18:05:36 +0100

    update group cache wrapper

--- engine/src/main/java/com/agiletec/aps/system/services/group/GroupManager.java
+++ engine/src/main/java/com/agiletec/aps/system/services/group/GroupManager.java
@@ -25,18 +25,19 @@ import com.agiletec.aps.system.common.AbstractService;
 
 /**
  * Servizio gestore dei gruppi.
+ *
  * @author E.Santoboni
  */
-public class GroupManager extends AbstractService implements IGroupManager  {
-	
+public class GroupManager extends AbstractService implements IGroupManager {
+
 	private static final Logger _logger = LoggerFactory.getLogger(GroupManager.class);
-	
+
 	@Override
 	public void init() throws Exception {
 		this.loadGroups();
 		_logger.debug("{} ready. Initialized {}  user groups", this.getClass().getName(), _groups.size());
 	}
-	
+
 	private void loadGroups() throws ApsSystemException {
 		try {
 			_groups = this.getGroupDAO().loadGroups();
@@ -44,9 +45,10 @@ public class GroupManager extends AbstractService implements IGroupManager  {
 			throw new ApsSystemException("Error loading the group list", t);
 		}
 	}
-	
+
 	/**
 	 * Aggiunge un gruppo nel sistema.
+	 *
 	 * @param group Il gruppo da aggiungere.
 	 * @throws ApsSystemException In caso di errori in accesso al db.
 	 */
@@ -54,15 +56,16 @@ public class GroupManager extends AbstractService implements IGroupManager  {
 	public void addGroup(Group group) throws ApsSystemException {
 		try {
 			this.getGroupDAO().addGroup(group);
-			_groups.put(group.getName(), group );
+			_groups.put(group.getName(), group);
 		} catch (Throwable t) {
 			_logger.error("Error detected while adding a group", t);
 			throw new ApsSystemException("Error detected while adding a group", t);
 		}
 	}
-	
+
 	/**
 	 * Rimuove un gruppo dal sistema.
+	 *
 	 * @param group Il gruppo da rimuovere.
 	 * @throws ApsSystemException In caso di errori in accesso al db.
 	 */
@@ -76,9 +79,10 @@ public class GroupManager extends AbstractService implements IGroupManager  {
 			throw new ApsSystemException("Error while removing a group", t);
 		}
 	}
-	
+
 	/**
 	 * Aggiorna un gruppo di sistema.
+	 *
 	 * @param group Il gruppo da aggiornare.
 	 * @throws ApsSystemException In caso di errori in accesso al db.
 	 */
@@ -87,34 +91,37 @@ public class GroupManager extends AbstractService implements IGroupManager  {
 		try {
 			this.getGroupDAO().updateGroup(group);
 			Group groupInMap = this.getGroup(group.getName());
-			groupInMap.setDescr(group.getDescr());
+			groupInMap.setDescription(group.getDescription());
 		} catch (Throwable t) {
 			_logger.error("Error updating a group", t);
 			throw new ApsSystemException("Error updating a group", t);
 		}
 	}
-	
+
 	/**
 	 * Restituisce la lista dei gruppi presenti nel sistema.
+	 *
 	 * @return La lista dei gruppi presenti nel sistema.
 	 */
 	@Override
 	public List<Group> getGroups() {
 		return new ArrayList<Group>(_groups.values());
 	}
-	
+
 	/**
-	 * Restituisce la mappa dei gruppi presenti nel sistema. 
-	 * La mappa è indicizzata in base al nome del gruppo.
+	 * Restituisce la mappa dei gruppi presenti nel sistema. La mappa è
+	 * indicizzata in base al nome del gruppo.
+	 *
 	 * @return La mappa dei gruppi presenti nel sistema.
 	 */
 	@Override
 	public Map<String, Group> getGroupsMap() {
 		return _groups;
 	}
-	
+
 	/**
 	 * Restituisce un gruppo in base al nome.
+	 *
 	 * @param groupName Il nome del gruppo.
 	 * @return Il gruppo cercato.
 	 */
@@ -122,20 +129,21 @@ public class GroupManager extends AbstractService implements IGroupManager  {
 	public Group getGroup(String groupName) {
 		return (Group) this._groups.get(groupName);
 	}
-	
+
 	protected IGroupDAO getGroupDAO() {
 		return this._groupDao;
 	}
+
 	public void setGroupDAO(IGroupDAO groupDao) {
 		this._groupDao = groupDao;
 	}
-	
+
 	/**
-	 * La mappa dei gruppi presenti nel sistema 
-	 * indicizzata in base al nome del gruppo.
+	 * La mappa dei gruppi presenti nel sistema indicizzata in base al nome del
+	 * gruppo.
 	 */
 	private Map<String, Group> _groups;
-	
+
 	private IGroupDAO _groupDao;
-	
+
 }

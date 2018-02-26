d60820e2da8d07c9bc7b1ad1b466e883e77eac28
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-02-26 11:42:03 +0100

    EN-802 Upgrade Fragment rest endpoint

--- engine/src/main/java/org/entando/entando/aps/system/services/guifragment/model/GuiFragmentDto.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/guifragment/model/GuiFragmentDto.java
@@ -13,11 +13,223 @@
  */
 package org.entando.entando.aps.system.services.guifragment.model;
 
+import com.agiletec.aps.system.services.pagemodel.PageModel;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.entando.entando.aps.system.services.guifragment.GuiFragment;
+import org.entando.entando.aps.system.services.widgettype.WidgetType;
+
 /**
- *
- * @author eu
+ * @author E.Santoboni
  */
 public class GuiFragmentDto {
 
-	//TO COMPLETE
+	private String code;
+	private boolean locked;
+	private WidgetTypeRef widgetType;
+	private PluginRef plugin;
+	private List<FragmentRef> fragments = new ArrayList<>();
+	private List<PageModelRef> pageModels = new ArrayList<>();
+	private String defaultGuiCode;
+	private String guiCode;
+
+	public GuiFragmentDto() {
+	}
+
+	public GuiFragmentDto(GuiFragment guiFragment, WidgetType type) {
+		this.setCode(guiFragment.getCode());
+		this.setLocked(guiFragment.isLocked());
+		if (!StringUtils.isEmpty(guiFragment.getWidgetTypeCode())) {
+			WidgetTypeRef widgetType = new WidgetTypeRef(guiFragment.getWidgetTypeCode(), null);
+			this.setWidgetType(widgetType);
+		}
+		if (!StringUtils.isEmpty(guiFragment.getPluginCode())) {
+			PluginRef plugin = new PluginRef(guiFragment.getPluginCode(), null);
+			this.setPlugin(plugin);
+		}
+		this.setDefaultGuiCode(guiFragment.getDefaultGui());
+		this.setGuiCode(guiFragment.getGui());
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public boolean isLocked() {
+		return locked;
+	}
+
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+
+	public WidgetTypeRef getWidgetType() {
+		return widgetType;
+	}
+
+	public void setWidgetType(WidgetTypeRef widgetType) {
+		this.widgetType = widgetType;
+	}
+
+	public PluginRef getPlugin() {
+		return plugin;
+	}
+
+	public void setPlugin(PluginRef plugin) {
+		this.plugin = plugin;
+	}
+
+	public void addFragmentRef(GuiFragment fragment) {
+		if (null == fragment) {
+			return;
+		}
+		FragmentRef ref = new FragmentRef(fragment.getCode());
+		this.getFragments().add(ref);
+	}
+
+	public List<FragmentRef> getFragments() {
+		return fragments;
+	}
+
+	public void setFragments(List<FragmentRef> fragments) {
+		this.fragments = fragments;
+	}
+
+	public void addPageModelRef(PageModel pageModel) {
+		if (null == pageModel) {
+			return;
+		}
+		PageModelRef ref = new PageModelRef(pageModel.getCode(), pageModel.getDescription());
+		this.getPageModels().add(ref);
+	}
+
+	public List<PageModelRef> getPageModels() {
+		return pageModels;
+	}
+
+	public void setPageModels(List<PageModelRef> pageModels) {
+		this.pageModels = pageModels;
+	}
+
+	public String getDefaultGuiCode() {
+		return defaultGuiCode;
+	}
+
+	public void setDefaultGuiCode(String defaultGuiCode) {
+		this.defaultGuiCode = defaultGuiCode;
+	}
+
+	public String getGuiCode() {
+		return guiCode;
+	}
+
+	public void setGuiCode(String guiCode) {
+		this.guiCode = guiCode;
+	}
+
+	protected class WidgetTypeRef {
+
+		private String code;
+		private String title;
+
+		public WidgetTypeRef(String code, String title) {
+			this.setCode(code);
+			this.setTitle(title);
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+
+	protected class PluginRef {
+
+		private String code;
+		private String title;
+
+		public PluginRef(String code, String title) {
+			this.setCode(code);
+			this.setTitle(title);
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+
+	protected class FragmentRef {
+
+		private String code;
+
+		public FragmentRef(String code) {
+			this.setCode(code);
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+	}
+
+	protected class PageModelRef {
+
+		private String code;
+		private String name;
+
+		public PageModelRef(String code, String name) {
+			this.setCode(code);
+			this.setName(name);
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+
 }
--- engine/src/main/java/org/entando/entando/aps/system/services/guifragment/model/GuiFragmentDtoBuilder.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/guifragment/model/GuiFragmentDtoBuilder.java
@@ -13,16 +13,79 @@
  */
 package org.entando.entando.aps.system.services.guifragment.model;
 
+import com.agiletec.aps.system.services.pagemodel.PageModel;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.entando.entando.aps.system.services.DtoBuilder;
 import org.entando.entando.aps.system.services.guifragment.GuiFragment;
+import org.entando.entando.aps.system.services.guifragment.GuiFragmentUtilizer;
+import org.entando.entando.aps.system.services.widgettype.IWidgetTypeManager;
+import org.entando.entando.aps.system.services.widgettype.WidgetType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.ListableBeanFactory;
 
-public class GuiFragmentDtoBuilder extends DtoBuilder<GuiFragment, GuiFragmentDto> {
+public class GuiFragmentDtoBuilder extends DtoBuilder<GuiFragment, GuiFragmentDto> implements BeanFactoryAware {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private BeanFactory beanFactory;
+	private IWidgetTypeManager widgetTypeManager;
 
 	@Override
 	protected GuiFragmentDto toDto(GuiFragment src) {
-		GuiFragmentDto dest = new GuiFragmentDto();
-		//TO COMPLETE
+		if (null == src) {
+			return null;
+		}
+		WidgetType type = null;
+		if (StringUtils.isNotEmpty(src.getWidgetTypeCode())) {
+			type = this.getWidgetTypeManager().getWidgetType(src.getWidgetTypeCode());
+		}
+		GuiFragmentDto dest = new GuiFragmentDto(src, type);
+		ListableBeanFactory factory = (ListableBeanFactory) this.beanFactory;
+		String[] defNames = factory.getBeanNamesForType(GuiFragmentUtilizer.class);
+		for (String defName : defNames) {
+			GuiFragmentUtilizer utilizers = null;
+			try {
+				utilizers = this.beanFactory.getBean(defName, GuiFragmentUtilizer.class);
+				List<Object> references = utilizers.getGuiFragmentUtilizers(src.getCode());
+				if (null != references) {
+					for (Object reference : references) {
+						if (reference instanceof GuiFragment) {
+							dest.addFragmentRef((GuiFragment) reference);
+						} else if (reference instanceof PageModel) {
+							dest.addPageModelRef((PageModel) reference);
+						} else {
+							logger.info("unexpected reference - type {}", reference.getClass());
+						}
+					}
+				}
+			} catch (Throwable t) {
+				logger.error("Error extracting reference from bean '{}'", defName);
+				utilizers = null;
+			}
+		}
 		return dest;
+	}
+
+	protected BeanFactory getBeanFactory() {
+		return beanFactory;
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory bf) throws BeansException {
+		this.beanFactory = bf;
+	}
+
+	protected IWidgetTypeManager getWidgetTypeManager() {
+		return widgetTypeManager;
+	}
+
+	public void setWidgetTypeManager(IWidgetTypeManager widgetTypeManager) {
+		this.widgetTypeManager = widgetTypeManager;
 	}
 
 }

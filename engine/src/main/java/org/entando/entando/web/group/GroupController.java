e582a99e74e3e019bd78e69534ebf24a807168f6
stefano puddu <stefano.puddu@realt.it>
2018-02-28 10:54:46 +0100

    removed @controller

--- engine/src/main/java/org/entando/entando/web/group/GroupController.java
+++ engine/src/main/java/org/entando/entando/web/group/GroupController.java
@@ -33,7 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +40,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
 @RequestMapping(value = "/groups")
 public class GroupController {

50dc383b00af7e2ff5fd06b37c9276447db9ccf1
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-01 16:29:15 +0100

    EN-861 Testing update

--- engine/src/test/java/org/entando/entando/aps/system/services/group/GroupServiceIntegrationTest.java
+++ engine/src/test/java/org/entando/entando/aps/system/services/group/GroupServiceIntegrationTest.java
@@ -11,106 +11,105 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class GroupServiceIntegrationTest extends BaseTestCase {
 
-    private IGroupService groupService;
+	private IGroupService groupService;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        this.init();
-    }
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.init();
+	}
 
-    private void init() throws Exception {
-        try {
-            groupService = (IGroupService) this.getApplicationContext().getBean(IGroupService.BEAN_NAME);
-        } catch (Exception e) {
-            throw e;
-        }
-    }
+	private void init() throws Exception {
+		try {
+			groupService = (IGroupService) this.getApplicationContext().getBean(IGroupService.BEAN_NAME);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 
-    @Test
-    public void testGetGroups() throws JsonProcessingException {
-        RestListRequest restListRequest = new RestListRequest();
-        restListRequest.setPageSize(5);
-        PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(0));
-        assertThat(res.getSize(), is(5));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(6));
+	@Test
+	public void testGetGroups() throws JsonProcessingException {
+		RestListRequest restListRequest = new RestListRequest();
+		restListRequest.setPageSize(5);
+		PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(0));
+		assertThat(res.getPageSize(), is(5));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(6));
 
-        //
-        restListRequest.setPageSize(2);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(0));
-        assertThat(res.getSize(), is(2));
-        assertThat(res.getLast(), is(2));
-        assertThat(res.getCount(), is(6));
+		//
+		restListRequest.setPageSize(2);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(0));
+		assertThat(res.getPageSize(), is(2));
+		assertThat(res.getLastPage(), is(2));
+		assertThat(res.getCount(), is(6));
 
-        //
-        restListRequest.setPageSize(4);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(0));
-        assertThat(res.getSize(), is(4));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(6));
+		//
+		restListRequest.setPageSize(4);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(0));
+		assertThat(res.getPageSize(), is(4));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(6));
 
-        //
-        restListRequest.setPageSize(4);
-        restListRequest.setPageNum(1);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(1));
-        assertThat(res.getSize(), is(2));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(6));
-        //
-        restListRequest.setPageSize(4);
-        restListRequest.setPageNum(1000);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(1000));
-        assertThat(res.getSize(), is(0));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(6));
-    }
+		//
+		restListRequest.setPageSize(4);
+		restListRequest.setPageNum(1);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(1));
+		assertThat(res.getPageSize(), is(2));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(6));
+		//
+		restListRequest.setPageSize(4);
+		restListRequest.setPageNum(1000);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(1000));
+		assertThat(res.getPageSize(), is(0));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(6));
+	}
 
-    @Test
-    public void testGetGroups_filter() throws JsonProcessingException {
-        RestListRequest restListRequest = new RestListRequest();
-        restListRequest.addFilter(new Filter("groupname", "fr"));
+	@Test
+	public void testGetGroups_filter() throws JsonProcessingException {
+		RestListRequest restListRequest = new RestListRequest();
+		restListRequest.addFilter(new Filter("groupname", "fr"));
 
-        PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(0));
-        assertThat(res.getSize(), is(1));
-        assertThat(res.getLast(), is(0));
-        assertThat(res.getCount(), is(1));
-    }
+		PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(0));
+		assertThat(res.getPageSize(), is(1));
+		assertThat(res.getLastPage(), is(0));
+		assertThat(res.getCount(), is(1));
+	}
 
-    @Test
-    public void testGetGroups_filter_and_pagination() throws JsonProcessingException {
-        RestListRequest restListRequest = new RestListRequest();
-        restListRequest.setPageSize(2);
-        restListRequest.addFilter(new Filter("groupname", "s"));
+	@Test
+	public void testGetGroups_filter_and_pagination() throws JsonProcessingException {
+		RestListRequest restListRequest = new RestListRequest();
+		restListRequest.setPageSize(2);
+		restListRequest.addFilter(new Filter("groupname", "s"));
 
-        PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(0));
-        assertThat(res.getSize(), is(2));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(3));
+		PagedMetadata<GroupDto> res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(0));
+		assertThat(res.getPageSize(), is(2));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(3));
 
-        restListRequest.setPageNum(1);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(1));
-        assertThat(res.getSize(), is(1));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(3));
+		restListRequest.setPageNum(1);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(1));
+		assertThat(res.getPageSize(), is(1));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(3));
 
-        restListRequest.setPageNum(2);
-        res = this.groupService.getGroups(restListRequest);
-        assertThat(res.getPage(), is(2));
-        assertThat(res.getSize(), is(0));
-        assertThat(res.getLast(), is(1));
-        assertThat(res.getCount(), is(3));
-    }
+		restListRequest.setPageNum(2);
+		res = this.groupService.getGroups(restListRequest);
+		assertThat(res.getPage(), is(2));
+		assertThat(res.getPageSize(), is(0));
+		assertThat(res.getLastPage(), is(1));
+		assertThat(res.getCount(), is(3));
+	}
 
 }

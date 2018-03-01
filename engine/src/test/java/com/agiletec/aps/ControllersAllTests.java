d9c8abeb810c52928b8afa7ef9cd8f68dcb00670
Pasquale Addeo <p.addeo@entando.com>
2018-03-01 12:29:12 +0100

    En 674 pages endpoints (#322)
    * page endpoints
    
    * page controller fix
    
    * unit tests
    
    * log removed
    
    * TODOs done

--- engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
+++ engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
@@ -6,17 +6,19 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.entando.entando.web.group.GroupControllerTest;
 import org.entando.entando.web.guifragment.GuiFragmentControllerTest;
+import org.entando.entando.web.page.PageControllerTest;
 import org.entando.entando.web.pagesettings.PageSettingsControllerTest;
 
 public class ControllersAllTests extends TestCase {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite(ControllersAllTests.class.getName());
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ControllersAllTests.class.getName());
 
-		suite.addTest(new JUnit4TestAdapter(GroupControllerTest.class));
-		suite.addTest(new JUnit4TestAdapter(PageSettingsControllerTest.class));
-		suite.addTest(new JUnit4TestAdapter(GuiFragmentControllerTest.class));
-		return suite;
-	}
+        suite.addTest(new JUnit4TestAdapter(GroupControllerTest.class));
+        suite.addTest(new JUnit4TestAdapter(PageSettingsControllerTest.class));
+        suite.addTest(new JUnit4TestAdapter(PageControllerTest.class));
+      	suite.addTest(new JUnit4TestAdapter(GuiFragmentControllerTest.class));
+        return suite;
+    }
 
 }

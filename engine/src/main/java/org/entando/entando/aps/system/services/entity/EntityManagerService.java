8e2ac1eb1f53c8f559af6239779a5d15daf00b70
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-06 17:30:15 +0100

    EN-866 add skeleton of controller and service

---
+++ engine/src/main/java/org/entando/entando/aps/system/services/entity/EntityManagerService.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.entando.entando.aps.system.services.entity;
+
+import org.entando.entando.aps.system.services.entity.model.EntityManagerDto;
+import org.entando.entando.web.common.model.PagedMetadata;
+import org.entando.entando.web.common.model.RestListRequest;
+
+public class EntityManagerService implements IEntityManagerService {
+
+    @Override
+    public PagedMetadata<EntityManagerDto> getEntityManagers(RestListRequest requestList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public EntityManagerDto getEntityManager(String entityManagerCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}

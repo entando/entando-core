f500a3e70d1058b7e8faa085cc27bf8a7741429b
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-08 13:10:22 +0100

    EN-866 Add log (debug) for request and response

--- engine/src/main/java/org/entando/entando/web/entity/EntityManagerController.java
+++ engine/src/main/java/org/entando/entando/web/entity/EntityManagerController.java
@@ -14,12 +14,16 @@
 package org.entando.entando.web.entity;
 
 import com.agiletec.aps.system.services.role.Permission;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.entando.entando.aps.system.services.entity.IEntityManagerService;
 import org.entando.entando.aps.system.services.entity.model.EntityManagerDto;
 import org.entando.entando.web.common.annotation.RestAccessControl;
 import org.entando.entando.web.common.model.PagedMetadata;
 import org.entando.entando.web.common.model.RestListRequest;
 import org.entando.entando.web.common.model.RestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,6 +36,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/entityManagers")
 public class EntityManagerController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private IEntityManagerService entityManagerService;
@@ -46,15 +52,18 @@ public class EntityManagerController {
 
     @RestAccessControl(permission = Permission.SUPERUSER)
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getEntityManagers(RestListRequest requestList) {
+    public ResponseEntity<?> getEntityManagers(RestListRequest requestList) throws JsonProcessingException {
         PagedMetadata<String> result = this.getEntityManagerService().getEntityManagers(requestList);
+        logger.debug("Main Response -> " + new ObjectMapper().writeValueAsString(result));
         return new ResponseEntity<>(new RestResponse(result.getBody(), null, result), HttpStatus.OK);
     }
 
     @RestAccessControl(permission = Permission.SUPERUSER)
     @RequestMapping(value = "/{entityManagerCode}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getEntityManager(@PathVariable String entityManagerCode) {
+    public ResponseEntity<?> getEntityManager(@PathVariable String entityManagerCode) throws JsonProcessingException {
+        logger.debug("Requested manager -> " + entityManagerCode);
         EntityManagerDto dto = this.getEntityManagerService().getEntityManager(entityManagerCode);
+        logger.debug("Main Response -> " + new ObjectMapper().writeValueAsString(dto));
         return new ResponseEntity<>(new RestResponse(dto), HttpStatus.OK);
     }
 

e14dca4a6c0caecef5d45701ccf1dd4d6e14081a
paddeo <p.addeo@entando.com>
2018-03-13 11:28:42 +0100

    cors filter added

---
+++ engine/src/main/java/org/entando/entando/aps/servlet/CORSFilter.java
@@ -0,0 +1,36 @@
+package org.entando.entando.aps.servlet;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * @author paddeo
+ */
+public class CORSFilter extends OncePerRequestFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            logger.trace("Sending Header....");
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//			response.addHeader("Access-Control-Allow-Headers", "Authorization");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+            response.addHeader("Access-Control-Max-Age", "1");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}

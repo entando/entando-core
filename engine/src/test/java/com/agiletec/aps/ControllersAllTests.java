0260aa2b1ef26e07c07cf26b2f385d2b37047a68
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-16 13:34:55 +0100

    EN-865 add unit tests

--- engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
+++ engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package com.agiletec.aps;
 
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.entando.entando.web.dataobject.DataTypeControllerIntegrationTest;
+import org.entando.entando.web.dataobject.DataTypeControllerTest;
 import org.entando.entando.web.dataobjectmodel.DataObjectModelControllerTest;
 import org.entando.entando.web.entity.EntityManagerControllerTest;
 import org.entando.entando.web.group.GroupControllerIntegrationTest;
@@ -38,6 +53,10 @@ public class ControllersAllTests extends TestCase {
 
         suite.addTest(new JUnit4TestAdapter(LanguageControllerIntegrationTest.class));
         suite.addTest(new JUnit4TestAdapter(LanguageControllerUnitTest.class));
+
+        suite.addTest(new JUnit4TestAdapter(DataTypeControllerIntegrationTest.class));
+        suite.addTest(new JUnit4TestAdapter(DataTypeControllerTest.class));
+
         return suite;
     }
 

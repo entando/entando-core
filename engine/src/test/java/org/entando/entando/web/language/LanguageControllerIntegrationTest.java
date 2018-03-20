7978cd30cfbad19b555a70fbd313d53a4f1a213c
Nicola Puddu <n.puddu@entando.com>
2018-03-20 16:29:30 +0100

    fixed the missing tests

--- engine/src/test/java/org/entando/entando/web/language/LanguageControllerIntegrationTest.java
+++ engine/src/test/java/org/entando/entando/web/language/LanguageControllerIntegrationTest.java
@@ -52,7 +52,7 @@ public class LanguageControllerIntegrationTest extends AbstractControllerIntegra
          */
         result.andExpect(header().string("Access-Control-Allow-Origin", "*"));
         result.andExpect(header().string("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"));
-        result.andExpect(header().string("Access-Control-Allow-Headers", "Content-Type"));
+        result.andExpect(header().string("Access-Control-Allow-Headers", "Content-Type, Authorization"));
         result.andExpect(header().string("Access-Control-Max-Age", "3600"));
     }
 

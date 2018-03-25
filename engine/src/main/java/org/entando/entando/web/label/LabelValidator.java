0ca2ece86e90c965a339aad479b0d691fd1746e8
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-25 17:34:21 +0200

    EN-1073 fix labels get validation

--- engine/src/main/java/org/entando/entando/web/label/LabelValidator.java
+++ engine/src/main/java/org/entando/entando/web/label/LabelValidator.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package org.entando.entando.web.label;
 
 import org.apache.commons.lang3.StringUtils;
+import org.entando.entando.web.common.validator.AbstractValidator;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 @Component
-public class LabelValidator implements Validator {
+public class LabelValidator extends AbstractValidator {
 
     public static final String ERRCODE_LABELGROUP_EXISTS = "1";
 

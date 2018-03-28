8b83b9536082aab4fb6d16decd1e8e22204b44b1
paddeo <p.addeo@entando.com>
2018-03-28 17:59:05 +0200

    update user change and tests fix

--- admin-console/src/test/java/com/agiletec/apsadmin/common/TestBaseCommonAction.java
+++ admin-console/src/test/java/com/agiletec/apsadmin/common/TestBaseCommonAction.java
@@ -28,98 +28,99 @@ import com.opensymphony.xwork2.Action;
  */
 public class TestBaseCommonAction extends ApsAdminBaseTestCase {
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		this.init();
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.init();
+    }
 
-	public void testGoChangePasswordPage() throws Throwable {
-    	this.initAction("/do/CurrentUser", "editProfile");
-    	this.setUserOnSession("admin");
-    	String result = super.executeAction();
-		assertEquals(Action.SUCCESS, result);
-	}
+    public void testGoChangePasswordPage() throws Throwable {
+        this.initAction("/do/CurrentUser", "editProfile");
+        this.setUserOnSession("admin");
+        String result = super.executeAction();
+        assertEquals(Action.SUCCESS, result);
+    }
 
-	public void testFailureUpdate() throws Throwable {
-		String username = "editorCoach";
-		String rightOldPassword = "editorCoach";
-		this.setUserOnSession(username);
-		UserDetails oldUser = this._userManager.getUser(username);
-		try {
+    public void testFailureUpdate() throws Throwable {
+        String username = "editorCoach";
+        String rightOldPassword = "editorCoach";
+        this.setUserOnSession(username);
+        UserDetails oldUser = this._userManager.getUser(username);
+        try {
 
-			// oldPassword non valorizzata
-			String result = this.executeUpdate("", "password", "password");
-			this.verifyErrors(result, 1, "oldPassword", 1);
+            // oldPassword non valorizzata
+            String result = this.executeUpdate("", "password", "password");
+            this.verifyErrors(result, 1, "oldPassword", 1);
 
-			// oldPassword errata
-			result = this.executeUpdate("wrongOldPassword", "password", "password");
-			this.verifyErrors(result, 1, "oldPassword", 1);
+            // oldPassword errata
+            result = this.executeUpdate("wrongOldPassword", "password", "password");
+            this.verifyErrors(result, 1, "oldPassword", 1);
 
-			// Conferma errata
-			result = this.executeUpdate(rightOldPassword, "password", "badConfirm");
-			this.verifyErrors(result, 1, "password", 1);
+            // Conferma errata
+            result = this.executeUpdate(rightOldPassword, "password", "badConfirm");
+            this.verifyErrors(result, 1, "password", 1);
 
-			// password uguale alla vecchia
-			result = this.executeUpdate(rightOldPassword, rightOldPassword, rightOldPassword);
-			this.verifyErrors(result, 1, "password", 1);
+            // password uguale alla vecchia
+            result = this.executeUpdate(rightOldPassword, rightOldPassword, rightOldPassword);
+            this.verifyErrors(result, 1, "password", 1);
 
-			// password con caratteri non validi corta
-			result = this.executeUpdate(rightOldPassword, "p123%456$hj", "p123%456$hj");
-			this.verifyErrors(result, 1, "password", 1);
+            // password con caratteri non validi corta
+            result = this.executeUpdate(rightOldPassword, "p123%456$hj", "p123%456$hj");
+            this.verifyErrors(result, 1, "password", 1);
 
-			// password troppo corta
-			result = this.executeUpdate(rightOldPassword, "p", "p");
-			this.verifyErrors(result, 1, "password", 1);
+            // password troppo corta
+            result = this.executeUpdate(rightOldPassword, "p", "p");
+            this.verifyErrors(result, 1, "password", 1);
 
-			// password troppo lunga
-			result = this.executeUpdate(rightOldPassword, "passwordDecisamenteTroppoLunga", "passwordDecisamenteTroppoLunga");
-			this.verifyErrors(result, 1, "password", 1);
-		} catch (RuntimeException e) {
-			this._userManager.updateUser(oldUser);
-			throw e;
-		}
-	}
+            // password troppo lunga
+            result = this.executeUpdate(rightOldPassword, "passwordDecisamenteTroppoLunga", "passwordDecisamenteTroppoLunga");
+            this.verifyErrors(result, 1, "password", 1);
+        } catch (RuntimeException e) {
+            this._userManager.updateUser(oldUser);
+            this._userManager.changePassword(oldUser.getUsername(), oldUser.getUsername());
+            throw e;
+        }
+    }
 
-	private void verifyErrors(String result, int extectedFieldErrors, String fieldWithErrors, int expectedErrorsOnField) {
-		assertEquals(Action.INPUT, result);
-		Map<String, List<String>> fieldErrors = this.getAction().getFieldErrors();
-		assertEquals(extectedFieldErrors, fieldErrors.size());
-		List<String> errors = fieldErrors.get(fieldWithErrors);
-		assertEquals(expectedErrorsOnField, errors.size());
-	}
+    private void verifyErrors(String result, int extectedFieldErrors, String fieldWithErrors, int expectedErrorsOnField) {
+        assertEquals(Action.INPUT, result);
+        Map<String, List<String>> fieldErrors = this.getAction().getFieldErrors();
+        assertEquals(extectedFieldErrors, fieldErrors.size());
+        List<String> errors = fieldErrors.get(fieldWithErrors);
+        assertEquals(expectedErrorsOnField, errors.size());
+    }
 
-	public void testSuccessfulUpdate() throws Throwable {
-		String username = "editorCoach";
-		String rightOldPassword = "editorCoach";
-		String newPassword = "newPassword";
-		this.setUserOnSession(username);
-		UserDetails oldUser = this._userManager.getUser(username);
-		try {
-			String result = this.executeUpdate(rightOldPassword, newPassword, newPassword);
-			assertEquals(Action.SUCCESS, result);
+    public void testSuccessfulUpdate() throws Throwable {
+        String username = "editorCoach";
+        String rightOldPassword = "editorCoach";
+        String newPassword = "newPassword";
+        this.setUserOnSession(username);
+        UserDetails oldUser = this._userManager.getUser(username);
+        try {
+            String result = this.executeUpdate(rightOldPassword, newPassword, newPassword);
+            assertEquals(Action.SUCCESS, result);
 
-			UserDetails updatedUser = this._userManager.getUser(username, newPassword);
-			assertNotNull(updatedUser);
-		} catch (RuntimeException e) {
-			throw e;
-		} finally {
-			this._userManager.updateUser(oldUser);
-		}
-	}
+            UserDetails updatedUser = this._userManager.getUser(username, newPassword);
+            assertNotNull(updatedUser);
+        } catch (RuntimeException e) {
+            throw e;
+        } finally {
+            this._userManager.changePassword(oldUser.getUsername(), oldUser.getUsername());
+        }
+    }
 
-	private String executeUpdate(String oldPassword, String password, String passwordConfirm) throws Throwable {
-		this.initAction("/do/CurrentUser", "changePassword");
-		this.addParameter("oldPassword", oldPassword);
-		this.addParameter("password", password);
-		this.addParameter("passwordConfirm", passwordConfirm);
-		return this.executeAction();
-	}
+    private String executeUpdate(String oldPassword, String password, String passwordConfirm) throws Throwable {
+        this.initAction("/do/CurrentUser", "changePassword");
+        this.addParameter("oldPassword", oldPassword);
+        this.addParameter("password", password);
+        this.addParameter("passwordConfirm", passwordConfirm);
+        return this.executeAction();
+    }
 
-	private void init() {
-		this._userManager = (IUserManager) this.getService(SystemConstants.USER_MANAGER);
-	}
+    private void init() {
+        this._userManager = (IUserManager) this.getService(SystemConstants.USER_MANAGER);
+    }
 
-	private IUserManager _userManager;
+    private IUserManager _userManager;
 
 }
--- engine/src/test/java/org/entando/entando/aps/system/services/userprofile/TestUserManager.java
+++ engine/src/test/java/org/entando/entando/aps/system/services/userprofile/TestUserManager.java
@@ -29,120 +29,121 @@ import com.agiletec.aps.system.services.user.UserDetails;
  * @author E.Santoboni
  */
 public class TestUserManager extends BaseTestCase {
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		this.init();
-	}
-	
-	public void testInitialize() {
-		assertNotNull(this._userManager);
-		assertNotNull(this._profileManager);
-	}
-	
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.init();
+    }
+
+    public void testInitialize() {
+        assertNotNull(this._userManager);
+        assertNotNull(this._profileManager);
+    }
+
     public void testAddDeleteUser() throws Throwable {
-    	String username = "UserForTest1";
-		MockUser user = this.createUserForTest(username);
-		Date birthdate = this.steBirthdate(1982, 10, 25);		
-		IUserProfile profile = this.createProfile("stefano", "puddu", "spuddu@agiletec.it", birthdate, "it");
-		user.setProfile(profile);
-		try {
-			this._userManager.removeUser(user);
-			UserDetails extractedUser = this._userManager.getUser(username);
-			assertNull(extractedUser);
-			this._userManager.addUser(user);
-			extractedUser = this._userManager.getUser(username);
-			assertEquals(user.getUsername(), extractedUser.getUsername());
-			assertNotNull(user.getProfile());
-			assertEquals("spuddu@agiletec.it", ((IUserProfile) user.getProfile()).getValue("email"));
-		} catch (Throwable t) {
-			throw t;
-		} finally {
-			this._userManager.removeUser(user);
-			UserDetails extractedUser = this._userManager.getUser(username);
-			assertNull(extractedUser);
-		}
-	}
-    
+        String username = "UserForTest1";
+        MockUser user = this.createUserForTest(username);
+        Date birthdate = this.steBirthdate(1982, 10, 25);
+        IUserProfile profile = this.createProfile("stefano", "puddu", "spuddu@agiletec.it", birthdate, "it");
+        user.setProfile(profile);
+        try {
+            this._userManager.removeUser(user);
+            UserDetails extractedUser = this._userManager.getUser(username);
+            assertNull(extractedUser);
+            this._userManager.addUser(user);
+            extractedUser = this._userManager.getUser(username);
+            assertEquals(user.getUsername(), extractedUser.getUsername());
+            assertNotNull(user.getProfile());
+            assertEquals("spuddu@agiletec.it", ((IUserProfile) user.getProfile()).getValue("email"));
+        } catch (Throwable t) {
+            throw t;
+        } finally {
+            this._userManager.removeUser(user);
+            UserDetails extractedUser = this._userManager.getUser(username);
+            assertNull(extractedUser);
+        }
+    }
+
     public void testUpdateUser() throws Throwable {
-    	String username = "UserForTest2";
-    	Date birthdate = this.steBirthdate(1982, 10, 25);
-		MockUser user = this.createUserForTest(username);
-		IUserProfile profile = this.createProfile( "stefano", "puddu", "spuddu@agiletec.it", birthdate, "it");
-		user.setProfile(profile);
-		try {
-			this._userManager.removeUser(user);
-			UserDetails extractedUser = this._userManager.getUser(username);
-			assertNull(extractedUser);
-			this._userManager.addUser(user);
-			extractedUser = this._userManager.getUser(username);
-			assertEquals(user.getUsername(), extractedUser.getUsername());
-			assertNotNull(extractedUser);
-			assertEquals(0, extractedUser.getAuthorizations().size());
-			user.setPassword("changedPassword");
-			IUserProfile extractedProfile = (IUserProfile) extractedUser.getProfile();
-			assertNotNull(extractedProfile);
-			MonoTextAttribute emailAttr = (MonoTextAttribute) ((IUserProfile) extractedProfile).getAttribute("email");
-			assertEquals("spuddu@agiletec.it", emailAttr.getText());
-			emailAttr.setText("agiletectest@gmail.com");
-			user.setProfile(extractedProfile);
-			this._userManager.updateUser(user);
-			extractedUser = this._userManager.getUser(username);
-			assertEquals(user.getUsername(), extractedUser.getUsername());
-			extractedUser = this._userManager.getUser(username);
-			assertNotNull(extractedUser);
-			extractedProfile = (IUserProfile) extractedUser.getProfile();
-			MonoTextAttribute extractedEmailAttr = (MonoTextAttribute) ((IUserProfile) extractedProfile).getAttribute("email");
-			assertEquals("agiletectest@gmail.com", extractedEmailAttr.getText());
-		} catch (Throwable t) {
-			throw t;
-		} finally {
-			this._userManager.removeUser(user);
-			UserDetails extractedUser = this._userManager.getUser(username);
-			assertNull(extractedUser);
-		}
-	}
-	
-	private IUserProfile createProfile(String name,	String surname, String email, Date birthdate, String language) {
-		IUserProfile profile = _profileManager.getDefaultProfileType();
-		MonoTextAttribute nameAttr = (MonoTextAttribute) profile.getAttribute("fullname");
-		nameAttr.setText(name + " " + surname);
-		MonoTextAttribute emailAttr = (MonoTextAttribute) profile.getAttribute("email");
-		DateAttribute birthdateAttr = (DateAttribute) profile.getAttribute("birthdate");
-		birthdateAttr.setDate(birthdate);
-		MonoTextAttribute languageAttr = (MonoTextAttribute) profile.getAttribute("language");
-		languageAttr.setText(language);
-		emailAttr.setText(email);
-		return profile;
-	}
-	
-	private Date steBirthdate(int year, int month, int day){
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.YEAR, year);
-		calendar.set(Calendar.MONTH, month);
-		calendar.set(Calendar.DAY_OF_MONTH, day);
-		Date birthdate = new Date(calendar.getTimeInMillis());
-		return birthdate;
-	}
-    
+        String username = "UserForTest2";
+        Date birthdate = this.steBirthdate(1982, 10, 25);
+        MockUser user = this.createUserForTest(username);
+        IUserProfile profile = this.createProfile("stefano", "puddu", "spuddu@agiletec.it", birthdate, "it");
+        user.setProfile(profile);
+        try {
+            this._userManager.removeUser(user);
+            UserDetails extractedUser = this._userManager.getUser(username);
+            assertNull(extractedUser);
+            this._userManager.addUser(user);
+            extractedUser = this._userManager.getUser(username);
+            assertEquals(user.getUsername(), extractedUser.getUsername());
+            assertNotNull(extractedUser);
+            assertEquals(0, extractedUser.getAuthorizations().size());
+            user.setPassword("changedPassword");
+            IUserProfile extractedProfile = (IUserProfile) extractedUser.getProfile();
+            assertNotNull(extractedProfile);
+            MonoTextAttribute emailAttr = (MonoTextAttribute) ((IUserProfile) extractedProfile).getAttribute("email");
+            assertEquals("spuddu@agiletec.it", emailAttr.getText());
+            emailAttr.setText("agiletectest@gmail.com");
+            user.setProfile(extractedProfile);
+            this._userManager.updateUser(user);
+            this._userManager.changePassword(user.getUsername(), user.getUsername());
+            extractedUser = this._userManager.getUser(username);
+            assertEquals(user.getUsername(), extractedUser.getUsername());
+            extractedUser = this._userManager.getUser(username);
+            assertNotNull(extractedUser);
+            extractedProfile = (IUserProfile) extractedUser.getProfile();
+            MonoTextAttribute extractedEmailAttr = (MonoTextAttribute) ((IUserProfile) extractedProfile).getAttribute("email");
+            assertEquals("agiletectest@gmail.com", extractedEmailAttr.getText());
+        } catch (Throwable t) {
+            throw t;
+        } finally {
+            this._userManager.removeUser(user);
+            UserDetails extractedUser = this._userManager.getUser(username);
+            assertNull(extractedUser);
+        }
+    }
+
+    private IUserProfile createProfile(String name, String surname, String email, Date birthdate, String language) {
+        IUserProfile profile = _profileManager.getDefaultProfileType();
+        MonoTextAttribute nameAttr = (MonoTextAttribute) profile.getAttribute("fullname");
+        nameAttr.setText(name + " " + surname);
+        MonoTextAttribute emailAttr = (MonoTextAttribute) profile.getAttribute("email");
+        DateAttribute birthdateAttr = (DateAttribute) profile.getAttribute("birthdate");
+        birthdateAttr.setDate(birthdate);
+        MonoTextAttribute languageAttr = (MonoTextAttribute) profile.getAttribute("language");
+        languageAttr.setText(language);
+        emailAttr.setText(email);
+        return profile;
+    }
+
+    private Date steBirthdate(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        Date birthdate = new Date(calendar.getTimeInMillis());
+        return birthdate;
+    }
+
     protected MockUser createUserForTest(String username) {
-    	MockUser user = new MockUser();
-		user.setUsername(username);
+        MockUser user = new MockUser();
+        user.setUsername(username);
         user.setPassword("temp");
         return user;
-	}
-	
-	private void init() throws Exception {
-    	try {
-    		this._userManager = (IUserManager) this.getService(SystemConstants.USER_MANAGER);
-    		this._profileManager = (IUserProfileManager) this.getService(SystemConstants.USER_PROFILE_MANAGER);
-		} catch (Exception e) {
-			throw e;
-		}
     }
-	
-	private IUserProfileManager _profileManager;
-	private IUserManager _userManager;
-	
+
+    private void init() throws Exception {
+        try {
+            this._userManager = (IUserManager) this.getService(SystemConstants.USER_MANAGER);
+            this._profileManager = (IUserProfileManager) this.getService(SystemConstants.USER_PROFILE_MANAGER);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private IUserProfileManager _profileManager;
+    private IUserManager _userManager;
+
 }

c9593d8f7d9a7f679759f75dc479844fe43b0633
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-29 17:41:22 +0200

    EN-954 add new methods (updateParam and updateParams) in Config Manager # Conflicts: #	engine/src/main/java/com/agiletec/aps/system/services/baseconfig/BaseConfigManager.java

--- engine/src/main/java/com/agiletec/aps/system/services/baseconfig/BaseConfigManager.java
+++ engine/src/main/java/com/agiletec/aps/system/services/baseconfig/BaseConfigManager.java
@@ -13,20 +13,22 @@
  */
 package com.agiletec.aps.system.services.baseconfig;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.ServletContext;
 
 import com.agiletec.aps.system.SystemConstants;
 import com.agiletec.aps.system.common.AbstractService;
 import com.agiletec.aps.system.exception.ApsSystemException;
 import com.agiletec.aps.system.services.baseconfig.cache.IConfigManagerCacheWrapper;
 import de.mkammerer.argon2.Argon2Factory;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.servlet.ServletContext;
+import java.util.HashMap;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ServletContextAware;
 
 /**
@@ -43,191 +45,216 @@ import org.springframework.web.context.ServletContextAware;
  */
 public class BaseConfigManager extends AbstractService implements ConfigInterface, ServletContextAware {
 
-	private static final Logger logger = LoggerFactory.getLogger(BaseConfigManager.class);
+    private static final Logger logger = LoggerFactory.getLogger(BaseConfigManager.class);
 
-	private Map<String, String> systemParams;
+    private Map<String, String> systemParams;
 
-	private String securityConfigPath = null;
+    private String securityConfigPath = null;
 
-	private IConfigManagerCacheWrapper cacheWrapper;
+    private IConfigManagerCacheWrapper cacheWrapper;
 
-	private boolean argon2 = false;
+    private boolean argon2 = false;
 
-	private IConfigItemDAO configDao;
+    private IConfigItemDAO configDao;
 
-	private ServletContext servletContext;
+    private ServletContext servletContext;
 
-	@Override
-	public void init() throws Exception {
-		String version = this.getSystemParams().get(SystemConstants.INIT_PROP_CONFIG_VERSION);
-		this.getCacheWrapper().initCache(this.getConfigDAO(), version);
-		argon2 = (this.getParam("argon2") != null
-				&& this.getParam("argon2").equalsIgnoreCase("true"));
-		Properties props = this.extractSecurityConfiguration();
-		this.checkSecurityConfiguration(props);
-		logger.debug("{} ready. Initialized", this.getClass().getName());
-	}
+    @Override
+    public void init() throws Exception {
+        String version = this.getSystemParams().get(SystemConstants.INIT_PROP_CONFIG_VERSION);
+        this.getCacheWrapper().initCache(this.getConfigDAO(), version);
+        argon2 = (this.getParam("argon2") != null
+                && this.getParam("argon2").equalsIgnoreCase("true"));
+        Properties props = this.extractSecurityConfiguration();
+        this.checkSecurityConfiguration(props);
+        logger.debug("{} ready. Initialized", this.getClass().getName());
+    }
 
-	/**
-	 * Restituisce il valore di una voce della tabella di sistema. Il valore può
-	 * essere un XML complesso.
-	 *
-	 * @param name Il nome della voce di configurazione.
-	 * @return Il valore della voce di configurazione.
-	 */
-	@Override
-	public String getConfigItem(String name) {
-		return this.getCacheWrapper().getConfigItem(name);
-	}
+    /**
+     * Restituisce il valore di una voce della tabella di sistema. Il valore può
+     * essere un XML complesso.
+     *
+     * @param name Il nome della voce di configurazione.
+     * @return Il valore della voce di configurazione.
+     */
+    @Override
+    public String getConfigItem(String name) {
+        return this.getCacheWrapper().getConfigItem(name);
+    }
 
-	/**
-	 * Aggiorna un'item di configurazione nella mappa della configurazione degli
-	 * item e nel db.
-	 *
-	 * @param itemName Il nome dell'item da aggiornare.
-	 * @param config La nuova configurazione.
-	 * @throws ApsSystemException
-	 */
-	@Override
-	public void updateConfigItem(String itemName, String config) throws ApsSystemException {
-		String version = this.getSystemParams().get(SystemConstants.INIT_PROP_CONFIG_VERSION);
-		try {
-			this.getConfigDAO().updateConfigItem(itemName, config, version);
-			this.refresh();
-		} catch (Throwable t) {
-			logger.error("Error while updating item {}", itemName, t);
-			throw new ApsSystemException("Error while updating item", t);
-		}
-	}
+    /**
+     * Aggiorna un'item di configurazione nella mappa della configurazione degli
+     * item e nel db.
+     *
+     * @param itemName Il nome dell'item da aggiornare.
+     * @param config La nuova configurazione.
+     * @throws ApsSystemException
+     */
+    @Override
+    public void updateConfigItem(String itemName, String config) throws ApsSystemException {
+        String version = this.getSystemParams().get(SystemConstants.INIT_PROP_CONFIG_VERSION);
+        try {
+            this.getConfigDAO().updateConfigItem(itemName, config, version);
+            this.refresh();
+        } catch (Throwable t) {
+            logger.error("Error while updating item {}", itemName, t);
+            throw new ApsSystemException("Error while updating item", t);
+        }
+    }
 
-	/**
-	 * Restituisce il valore di un parametro di configurazione. I parametri sono
-	 * desunti dalla voce "params" della tabella di sistema.
-	 *
-	 * @param name Il nome del parametro di configurazione.
-	 * @return Il valore del parametro di configurazione.
-	 */
-	@Override
-	public String getParam(String name) {
-		String param = this.getSystemParams().get(name);
-		if (null != param) {
-			return param;
-		} else {
-			return this.getCacheWrapper().getParam(name);
-		}
-	}
+    /**
+     * Restituisce il valore di un parametro di configurazione. I parametri sono
+     * desunti dalla voce "params" della tabella di sistema.
+     *
+     * @param name Il nome del parametro di configurazione.
+     * @return Il valore del parametro di configurazione.
+     */
+    @Override
+    public String getParam(String name) {
+        String param = this.getSystemParams().get(name);
+        if (null != param) {
+            return param;
+        } else {
+            return this.getCacheWrapper().getParam(name);
+        }
+    }
 
-	protected Properties extractSecurityConfiguration() throws IOException {
-		Properties props = new Properties();
-		InputStream is = this.getServletContext().getResourceAsStream(this.getSecurityConfigPath());
-		if (null == is) {
-			throw new RuntimeException("Null security configuration inside " + this.getSecurityConfigPath());
-		}
-		props.load(is);
-		is.close();
-		return props;
-	}
+    protected Properties extractSecurityConfiguration() throws IOException {
+        Properties props = new Properties();
+        InputStream is = this.getServletContext().getResourceAsStream(this.getSecurityConfigPath());
+        if (null == is) {
+            throw new RuntimeException("Null security configuration inside " + this.getSecurityConfigPath());
+        }
+        props.load(is);
+        is.close();
+        return props;
+    }
 
-	protected void checkSecurityConfiguration(Properties mainProps) {
-		String algoType = null;
-		try {
-			algoType = Argon2Factory.Argon2Types.valueOf(mainProps.getProperty(ALGO_TYPE_PARAM_NAME)).name();
-		} catch (Exception e) {
-			String defaultAlgoType = Argon2Factory.Argon2Types.ARGON2i.name();
-			logger.error("Invalid value for Argon2 hashType '{}'; the default value is '{}'", algoType, defaultAlgoType, e);
-			throw new RuntimeException("Invalid value for Argon2 hashType '" + algoType + "'; the default value is '" + defaultAlgoType + "'", e);
-		}
-		System.getProperties().setProperty(ALGO_TYPE_PARAM_NAME, algoType);
+    protected void checkSecurityConfiguration(Properties mainProps) {
+        String algoType = null;
+        try {
+            algoType = Argon2Factory.Argon2Types.valueOf(mainProps.getProperty(ALGO_TYPE_PARAM_NAME)).name();
+        } catch (Exception e) {
+            String defaultAlgoType = Argon2Factory.Argon2Types.ARGON2i.name();
+            logger.error("Invalid value for Argon2 hashType '{}'; the default value is '{}'", algoType, defaultAlgoType, e);
+            throw new RuntimeException("Invalid value for Argon2 hashType '" + algoType + "'; the default value is '" + defaultAlgoType + "'", e);
+        }
+        System.getProperties().setProperty(ALGO_TYPE_PARAM_NAME, algoType);
 
-		Integer hashLength = Integer.valueOf(mainProps.getProperty(ALGO_HASH_LENGTH_PARAM_NAME));
-		if (hashLength < 4) {
-			throw new RuntimeException("Hash length must be greater than 4 - value '" + hashLength + "'");
-		}
-		System.getProperties().setProperty(ALGO_HASH_LENGTH_PARAM_NAME, String.valueOf(hashLength));
+        Integer hashLength = Integer.valueOf(mainProps.getProperty(ALGO_HASH_LENGTH_PARAM_NAME));
+        if (hashLength < 4) {
+            throw new RuntimeException("Hash length must be greater than 4 - value '" + hashLength + "'");
+        }
+        System.getProperties().setProperty(ALGO_HASH_LENGTH_PARAM_NAME, String.valueOf(hashLength));
 
-		Integer saltLength = Integer.valueOf(mainProps.getProperty(ALGO_SALT_LENGTH_PARAM_NAME));
-		if (saltLength < 8) {
-			throw new RuntimeException("Salt length must be greater than 8 - value '" + saltLength + "'");
-		}
-		System.getProperties().setProperty(ALGO_SALT_LENGTH_PARAM_NAME, String.valueOf(saltLength));
+        Integer saltLength = Integer.valueOf(mainProps.getProperty(ALGO_SALT_LENGTH_PARAM_NAME));
+        if (saltLength < 8) {
+            throw new RuntimeException("Salt length must be greater than 8 - value '" + saltLength + "'");
+        }
+        System.getProperties().setProperty(ALGO_SALT_LENGTH_PARAM_NAME, String.valueOf(saltLength));
 
-		Integer iterations = Integer.valueOf(mainProps.getProperty(ALGO_ITERATIONS_PARAM_NAME));
-		if (iterations < 1) {
-			throw new RuntimeException("Iterations number must be greater than 1 - value '" + iterations + "'");
-		}
-		System.getProperties().setProperty(ALGO_ITERATIONS_PARAM_NAME, String.valueOf(iterations));
+        Integer iterations = Integer.valueOf(mainProps.getProperty(ALGO_ITERATIONS_PARAM_NAME));
+        if (iterations < 1) {
+            throw new RuntimeException("Iterations number must be greater than 1 - value '" + iterations + "'");
+        }
+        System.getProperties().setProperty(ALGO_ITERATIONS_PARAM_NAME, String.valueOf(iterations));
 
-		Integer parallelism = Integer.valueOf(mainProps.getProperty(ALGO_PARALLELISM_PARAM_NAME));
-		if (parallelism < 1) {
-			throw new RuntimeException("Parallelism number must be greater than 1 - value '" + parallelism + "'");
-		}
-		System.getProperties().setProperty(ALGO_PARALLELISM_PARAM_NAME, String.valueOf(parallelism));
+        Integer parallelism = Integer.valueOf(mainProps.getProperty(ALGO_PARALLELISM_PARAM_NAME));
+        if (parallelism < 1) {
+            throw new RuntimeException("Parallelism number must be greater than 1 - value '" + parallelism + "'");
+        }
+        System.getProperties().setProperty(ALGO_PARALLELISM_PARAM_NAME, String.valueOf(parallelism));
 
-		Integer memory = Integer.valueOf(mainProps.getProperty(ALGO_MEMORY_PARAM_NAME));
-		if (memory < (8 * parallelism)) {
-			throw new RuntimeException("Memory size must be greater than 8xparallelism - value '" + memory + "'");
-		}
-		System.getProperties().setProperty(ALGO_MEMORY_PARAM_NAME, String.valueOf(memory));
-	}
+        Integer memory = Integer.valueOf(mainProps.getProperty(ALGO_MEMORY_PARAM_NAME));
+        if (memory < (8 * parallelism)) {
+            throw new RuntimeException("Memory size must be greater than 8xparallelism - value '" + memory + "'");
+        }
+        System.getProperties().setProperty(ALGO_MEMORY_PARAM_NAME, String.valueOf(memory));
+    }
 
-	/**
-	 * Restituisce il dao in uso al manager.
-	 *
-	 * @return Il dao in uso al manager.
-	 */
-	protected IConfigItemDAO getConfigDAO() {
-		return configDao;
-	}
+    @Override
+    public void updateParam(String name, String value) throws ApsSystemException {
+        if (StringUtils.isEmpty(name) || StringUtils.isEmpty(value)) {
+            return;
+        }
+        Map<String, String> params = new HashMap<>();
+        params.put(name, value);
+        this.updateParams(params);
+    }
 
-	/**
-	 * Setta il dao in uso al manager.
-	 *
-	 * @param configDao Il dao in uso al manager.
-	 */
-	public void setConfigDAO(IConfigItemDAO configDao) {
-		this.configDao = configDao;
-	}
+    @Override
+    public void updateParams(Map<String, String> params) throws ApsSystemException {
+        if (null == params) {
+            return;
+        }
+        try {
+            String xmlParams = this.getConfigItem(SystemConstants.CONFIG_ITEM_PARAMS);
+            String newXmlParams = SystemParamsUtils.getNewXmlParams(xmlParams, params, false);
+            this.updateConfigItem(SystemConstants.CONFIG_ITEM_PARAMS, newXmlParams);
+        } catch (Exception e) {
+            logger.error("Error while updating parameters {}", params, e);
+            throw new ApsSystemException("Error while updating parameters", e);
+        }
+    }
 
-	protected Map<String, String> getSystemParams() {
-		return this.systemParams;
-	}
+    /**
+     * Restituisce il dao in uso al manager.
+     *
+     * @return Il dao in uso al manager.
+     */
+    protected IConfigItemDAO getConfigDAO() {
+        return configDao;
+    }
 
-	public void setSystemParams(Map<String, String> systemParams) {
-		this.systemParams = systemParams;
-	}
+    /**
+     * Setta il dao in uso al manager.
+     *
+     * @param configDao Il dao in uso al manager.
+     */
+    public void setConfigDAO(IConfigItemDAO configDao) {
+        this.configDao = configDao;
+    }
 
-	protected String getSecurityConfigPath() {
-		return securityConfigPath;
-	}
+    protected Map<String, String> getSystemParams() {
+        return this.systemParams;
+    }
 
-	public void setSecurityConfigPath(String securityConfigPath) {
-		this.securityConfigPath = securityConfigPath;
-	}
+    public void setSystemParams(Map<String, String> systemParams) {
+        this.systemParams = systemParams;
+    }
 
-	protected IConfigManagerCacheWrapper getCacheWrapper() {
-		return cacheWrapper;
-	}
+    protected String getSecurityConfigPath() {
+        return securityConfigPath;
+    }
 
-	public void setCacheWrapper(IConfigManagerCacheWrapper cacheWrapper) {
-		this.cacheWrapper = cacheWrapper;
-	}
+    public void setSecurityConfigPath(String securityConfigPath) {
+        this.securityConfigPath = securityConfigPath;
+    }
 
-	@Override
-	public boolean isArgon2() {
-		return argon2;
-	}
+    protected IConfigManagerCacheWrapper getCacheWrapper() {
+        return cacheWrapper;
+    }
 
-	public void setArgon2(boolean argon2) {
-		this.argon2 = argon2;
-	}
+    public void setCacheWrapper(IConfigManagerCacheWrapper cacheWrapper) {
+        this.cacheWrapper = cacheWrapper;
+    }
 
-	protected ServletContext getServletContext() {
-		return servletContext;
-	}
+    @Override
+    public boolean isArgon2() {
+        return argon2;
+    }
 
-	@Override
-	public void setServletContext(ServletContext servletContext) {
-		this.servletContext = servletContext;
-	}
+    public void setArgon2(boolean argon2) {
+        this.argon2 = argon2;
+    }
+
+    protected ServletContext getServletContext() {
+        return servletContext;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
 
 }
--- engine/src/main/java/com/agiletec/aps/system/services/baseconfig/cache/ConfigManagerCacheWrapper.java
+++ engine/src/main/java/com/agiletec/aps/system/services/baseconfig/cache/ConfigManagerCacheWrapper.java
@@ -17,15 +17,11 @@ import com.agiletec.aps.system.SystemConstants;
 import com.agiletec.aps.system.common.AbstractCacheWrapper;
 import com.agiletec.aps.system.exception.ApsSystemException;
 import com.agiletec.aps.system.services.baseconfig.IConfigItemDAO;
-import com.agiletec.aps.util.MapSupportRule;
-import java.io.StringReader;
+import com.agiletec.aps.system.services.baseconfig.SystemParamsUtils;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.digester.Digester;
-import org.apache.commons.digester.Rule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.Cache;
@@ -35,84 +31,67 @@ import org.springframework.cache.Cache;
  */
 public class ConfigManagerCacheWrapper extends AbstractCacheWrapper implements IConfigManagerCacheWrapper {
 
-	private static final Logger logger = LoggerFactory.getLogger(ConfigManagerCacheWrapper.class);
+    private static final Logger logger = LoggerFactory.getLogger(ConfigManagerCacheWrapper.class);
 
-	@Override
-	public void initCache(IConfigItemDAO configItemDAO, String version) throws ApsSystemException {
-		try {
-			Cache cache = this.getCache();
-			this.releaseCachedObjects(cache);
-			Map<String, String> configItems = configItemDAO.loadVersionItems(version);
-			String xmlParams = configItems.get(SystemConstants.CONFIG_ITEM_PARAMS);
-			Map<String, String> params = this.parseParams(xmlParams);
-			this.insertObjectsOnCache(cache, configItems, params);
-		} catch (ApsSystemException t) {
-			logger.error("Error loading configuration params", t);
-			throw new ApsSystemException("Error loading configuration params", t);
-		}
-	}
+    @Override
+    public void initCache(IConfigItemDAO configItemDAO, String version) throws ApsSystemException {
+        try {
+            Cache cache = this.getCache();
+            this.releaseCachedObjects(cache);
+            Map<String, String> configItems = configItemDAO.loadVersionItems(version);
+            String xmlParams = configItems.get(SystemConstants.CONFIG_ITEM_PARAMS);
+            Map<String, String> params = SystemParamsUtils.getParams(xmlParams);
+            this.insertObjectsOnCache(cache, configItems, params);
+        } catch (Exception t) {
+            logger.error("Error loading configuration params", t);
+            throw new ApsSystemException("Error loading configuration params", t);
+        }
+    }
 
-	protected Map<String, String> parseParams(String xmlParams) throws ApsSystemException {
-		Map<String, String> params = new HashMap<>();
-		Digester dig = new Digester();
-		Rule rule = new MapSupportRule("name");
-		dig.addRule("*/Param", rule);
-		dig.push(params);
-		try {
-			dig.parse(new StringReader(xmlParams));
-		} catch (Exception e) {
-			logger.error("Error detected while parsing the \"params\" item in the \"sysconfig\" table: verify the \"sysconfig\" table", e);
-			throw new ApsSystemException(
-					"Error detected while parsing the \"params\" item in the \"sysconfig\" table:"
-					+ " verify the \"sysconfig\" table", e);
-		}
-		return params;
-	}
+    protected void releaseCachedObjects(Cache cache) {
+        this.releaseCachedObjects(cache, CONFIG_PARAMS_CODES_CACHE_NAME, CONFIG_PARAM_CACHE_NAME_PREFIX);
+        this.releaseCachedObjects(cache, CONFIG_ITEMS_CODES_CACHE_NAME, CONFIG_ITEM_CACHE_NAME_PREFIX);
+    }
 
-	protected void releaseCachedObjects(Cache cache) {
-		this.releaseCachedObjects(cache, CONFIG_PARAMS_CODES_CACHE_NAME, CONFIG_PARAM_CACHE_NAME_PREFIX);
-		this.releaseCachedObjects(cache, CONFIG_ITEMS_CODES_CACHE_NAME, CONFIG_ITEM_CACHE_NAME_PREFIX);
-	}
+    private void releaseCachedObjects(Cache cache, String codesName, String codePrefix) {
+        List<String> codes = (List<String>) this.get(cache, codesName, List.class);
+        if (null != codes) {
+            for (String code : codes) {
+                cache.evict(codePrefix + code);
+            }
+            cache.evict(codesName);
+        }
+    }
 
-	private void releaseCachedObjects(Cache cache, String codesName, String codePrefix) {
-		List<String> codes = (List<String>) this.get(cache, codesName, List.class);
-		if (null != codes) {
-			for (String code : codes) {
-				cache.evict(codePrefix + code);
-			}
-			cache.evict(codesName);
-		}
-	}
+    protected void insertObjectsOnCache(Cache cache, Map<String, String> configItems, Map<String, String> params) {
+        this.insertObjectsOnCache(cache, configItems, CONFIG_ITEMS_CODES_CACHE_NAME, CONFIG_ITEM_CACHE_NAME_PREFIX);
+        this.insertObjectsOnCache(cache, params, CONFIG_PARAMS_CODES_CACHE_NAME, CONFIG_PARAM_CACHE_NAME_PREFIX);
+    }
 
-	protected void insertObjectsOnCache(Cache cache, Map<String, String> configItems, Map<String, String> params) {
-		this.insertObjectsOnCache(cache, configItems, CONFIG_ITEMS_CODES_CACHE_NAME, CONFIG_ITEM_CACHE_NAME_PREFIX);
-		this.insertObjectsOnCache(cache, params, CONFIG_PARAMS_CODES_CACHE_NAME, CONFIG_PARAM_CACHE_NAME_PREFIX);
-	}
+    private void insertObjectsOnCache(Cache cache, Map<String, String> map, String codesCacheName, String codeCachePrefix) {
+        List<String> codes = new ArrayList<>();
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            cache.put(codeCachePrefix + key, map.get(key));
+            codes.add(key);
+        }
+        cache.put(codesCacheName, codes);
+    }
 
-	private void insertObjectsOnCache(Cache cache, Map<String, String> map, String codesCacheName, String codeCachePrefix) {
-		List<String> codes = new ArrayList<>();
-		Iterator<String> iterator = map.keySet().iterator();
-		while (iterator.hasNext()) {
-			String key = iterator.next();
-			cache.put(codeCachePrefix + key, map.get(key));
-			codes.add(key);
-		}
-		cache.put(codesCacheName, codes);
-	}
+    @Override
+    public String getConfigItem(String name) {
+        return this.get(CONFIG_ITEM_CACHE_NAME_PREFIX + name, String.class);
+    }
 
-	@Override
-	public String getConfigItem(String name) {
-		return this.get(CONFIG_ITEM_CACHE_NAME_PREFIX + name, String.class);
-	}
+    @Override
+    public String getParam(String name) {
+        return this.get(CONFIG_PARAM_CACHE_NAME_PREFIX + name, String.class);
+    }
 
-	@Override
-	public String getParam(String name) {
-		return this.get(CONFIG_PARAM_CACHE_NAME_PREFIX + name, String.class);
-	}
-
-	@Override
-	protected String getCacheName() {
-		return CONFIG_MANAGER_CACHE_NAME;
-	}
+    @Override
+    protected String getCacheName() {
+        return CONFIG_MANAGER_CACHE_NAME;
+    }
 
 }

622b095dc67081f82c2db157fb14b63a548dc759
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-04-06 13:15:15 +0200

    EN-994 add skeleton of controller and service

---
+++ engine/src/main/java/org/entando/entando/aps/system/services/storage/FileBrowserService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-Present Entando S.r.l. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.entando.entando.aps.system.services.storage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author E.Santoboni
+ */
+public class FileBrowserService implements IFileBrowserService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private IStorageManager storageManager;
+
+    public IStorageManager getStorageManager() {
+        return storageManager;
+    }
+
+    public void setStorageManager(IStorageManager storageManager) {
+        this.storageManager = storageManager;
+    }
+
+}
---
+++ engine/src/main/java/org/entando/entando/web/filebrowser/FileBrowserController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package org.entando.entando.web.filebrowser;
+
+import org.entando.entando.aps.system.services.storage.IFileBrowserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author E.Santoboni
+ */
+@RestController
+@RequestMapping(value = "/fileBrowser")
+public class FileBrowserController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private IFileBrowserService fileBrowserService;
+
+}

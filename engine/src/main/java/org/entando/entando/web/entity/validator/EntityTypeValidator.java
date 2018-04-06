0f2cd7dcd07f5486e2bd82a916888e8444437176
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-04-06 12:26:09 +0200

    EN-1145 little refactoring

--- engine/src/main/java/org/entando/entando/web/entity/validator/EntityTypeValidator.java
+++ engine/src/main/java/org/entando/entando/web/entity/validator/EntityTypeValidator.java
@@ -29,8 +29,6 @@ public abstract class EntityTypeValidator extends AbstractPaginationValidator im
 
     public static final String ERRCODE_ENTITY_TYPE_ALREADY_EXISTS = "1";
 
-    //public static final String ERRCODE_ENTITY_TYPE_CODE_REQUIRED = "2";
-    //public static final String ERRCODE_ENTITY_TYPE_DESCR_REQUIRED = "3";
     public static final String ERRCODE_URINAME_MISMATCH = "6";
     public static final String ERRCODE_ENTITY_TYPE_REFERENCES = "7";
 
@@ -63,8 +61,6 @@ public abstract class EntityTypeValidator extends AbstractPaginationValidator im
 
     @Override
     public void validate(Object target, Errors errors) {
-        //EntityTypeDtoRequest request = (EntityTypeDtoRequest) target;
-        //this.validateBody(request, errors);
     }
 
     public int validateBodyName(String typeCode, EntityTypeDtoRequest request, Errors errors) {
@@ -76,22 +72,9 @@ public abstract class EntityTypeValidator extends AbstractPaginationValidator im
             errors.reject(ERRCODE_ENTITY_TYPE_DOES_NOT_EXIST, new String[]{typeCode}, "entityType.notExists");
             return 404;
         }
-        return 0;//this.validateBody(request, errors);
-    }
-
-    /*
-    private int validateBody(EntityTypeDtoRequest request, Errors errors) {
-        if (StringUtils.isBlank(request.getCode())) {
-            errors.reject(ERRCODE_ENTITY_TYPE_CODE_REQUIRED, new String[]{}, "entityType.code.notBlank");
-            return 400;
-        }
-        if (StringUtils.isBlank(request.getName())) {
-            errors.reject(ERRCODE_ENTITY_TYPE_DESCR_REQUIRED, new String[]{}, "entityType.name.notBlank");
-            return 400;
-        }
         return 0;
     }
-     */
+
     public boolean existType(String typeCode) {
         return (null != this.getEntityManager().getEntityPrototype(typeCode));
     }

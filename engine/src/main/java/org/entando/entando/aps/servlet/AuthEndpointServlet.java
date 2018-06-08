97576896f872d500e2364c024a423110bbe84005
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-06-08 09:31:12 +0200

    EN-1590 Add content type into OAuth2/access_token endpoint response, and add missing license (#507)

--- engine/src/main/java/org/entando/entando/aps/servlet/AuthEndpointServlet.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/AuthEndpointServlet.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2018-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 package org.entando.entando.aps.servlet;
 
 import com.agiletec.aps.system.SystemConstants;
@@ -102,12 +115,11 @@ public class AuthEndpointServlet extends HttpServlet {
             logger.error("IOException {} ", e);
         }
 
-
     }
 
     private boolean validateClient(final OAuthAuthzRequest oauthRequest, HttpServletRequest request, HttpServletResponse response) throws OAuthProblemException {
-        final IOAuthConsumerManager consumerManager =
-                (IOAuthConsumerManager) ApsWebApplicationUtils.getBean(SystemConstants.OAUTH_CONSUMER_MANAGER, request);
+        final IOAuthConsumerManager consumerManager
+                = (IOAuthConsumerManager) ApsWebApplicationUtils.getBean(SystemConstants.OAUTH_CONSUMER_MANAGER, request);
         final String clientId = oauthRequest.getClientId();
 
         try {

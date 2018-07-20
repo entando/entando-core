aa9cf32a55f39e3839be4efe8f9dc5e35b4cbbb5
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-07-20 10:48:30 +0200

    Fixed bug in front redirect servlet and added javadoc (#526)
    * Fixed bug in front redirect servlet and added javadoc
    
    * Refactoring and added test for FrontServletActionRedirect
    
    * enable test
    
    * Testing update
    
    * Testing update
    
    * Update page settings setrvice test
    
    # Conflicts:
    #	engine/src/test/java/org/entando/entando/aps/system/services/pagesettings/PageSettingsServiceIntegrationTest.java

--- engine/src/test/resources/sql/entandoPort_data_test.sql
+++ engine/src/test/resources/sql/entandoPort_data_test.sql
@@ -514,7 +514,7 @@ INSERT INTO sysconfig (version, item, descr, config) VALUES ('test', 'params', '
 	<Param name="treeStyle_category">classic</Param>
 	<Param name="startLangFromBrowser">false</Param>
 	<Param name="firstTimeMessages">true</Param>
-	<Param name="baseUrl">static</Param>
+	<Param name="baseUrl">request</Param>
 	<Param name="baseUrlContext">true</Param>
 	<Param name="useJsessionId">true</Param>
 	<SpecialPages>

70e59808d31016040025ba2f8fcbb1a32acb9042
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-07-26 17:55:50 +0200

    Improve cached object handling

--- engine/src/main/java/org/entando/entando/aps/system/services/widgettype/cache/WidgetTypeManagerCacheWrapper.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/widgettype/cache/WidgetTypeManagerCacheWrapper.java
@@ -29,68 +29,67 @@ import org.springframework.cache.Cache;
  */
 public class WidgetTypeManagerCacheWrapper extends AbstractGenericCacheWrapper<WidgetType> implements IWidgetTypeManagerCacheWrapper {
 
-	private static final Logger _logger = LoggerFactory.getLogger(WidgetTypeManagerCacheWrapper.class);
+    private static final Logger _logger = LoggerFactory.getLogger(WidgetTypeManagerCacheWrapper.class);
 
-	@Override
-	public void initCache(IWidgetTypeDAO widgetTypeDAO) throws ApsSystemException {
-		try {
-			Cache cache = this.getCache();
-			this.releaseCachedObjects(cache);
-			Map<String, WidgetType> widgetTypes = widgetTypeDAO.loadWidgetTypes();
-			Iterator<WidgetType> iter = widgetTypes.values().iterator();
-			while (iter.hasNext()) {
-				WidgetType type = iter.next();
-				String mainTypeCode = type.getParentTypeCode();
-				if (null != mainTypeCode) {
-					type.setParentType(widgetTypes.get(mainTypeCode));
-				}
-			}
-			this.insertObjectsOnCache(cache, widgetTypes);
-		} catch (Throwable t) {
-			_logger.error("Error loading widgets types", t);
-			throw new ApsSystemException("Error loading widgets types", t);
-		}
-	}
+    @Override
+    public void initCache(IWidgetTypeDAO widgetTypeDAO) throws ApsSystemException {
+        try {
+            Cache cache = this.getCache();
+            Map<String, WidgetType> widgetTypes = widgetTypeDAO.loadWidgetTypes();
+            Iterator<WidgetType> iter = widgetTypes.values().iterator();
+            while (iter.hasNext()) {
+                WidgetType type = iter.next();
+                String mainTypeCode = type.getParentTypeCode();
+                if (null != mainTypeCode) {
+                    type.setParentType(widgetTypes.get(mainTypeCode));
+                }
+            }
+            this.insertAndCleanCache(cache, widgetTypes);
+        } catch (Throwable t) {
+            _logger.error("Error loading widgets types", t);
+            throw new ApsSystemException("Error loading widgets types", t);
+        }
+    }
 
-	@Override
-	public WidgetType getWidgetType(String code) {
-		return this.get(WIDGET_TYPE_CACHE_NAME_PREFIX + code, WidgetType.class);
-	}
+    @Override
+    public WidgetType getWidgetType(String code) {
+        return this.get(WIDGET_TYPE_CACHE_NAME_PREFIX + code, WidgetType.class);
+    }
 
-	@Override
-	public Collection<WidgetType> getWidgetTypes() {
-		Map<String, WidgetType> map = super.getObjectMap();
-		return map.values();
-	}
+    @Override
+    public Collection<WidgetType> getWidgetTypes() {
+        Map<String, WidgetType> map = super.getObjectMap();
+        return map.values();
+    }
 
-	@Override
-	public void addWidgetType(WidgetType type) {
-		this.manage(type.getCode(), type, Action.ADD);
-	}
+    @Override
+    public void addWidgetType(WidgetType type) {
+        this.manage(type.getCode(), type, Action.ADD);
+    }
 
-	@Override
-	public void updateWidgetType(WidgetType type) {
-		this.manage(type.getCode(), type, Action.UPDATE);
-	}
+    @Override
+    public void updateWidgetType(WidgetType type) {
+        this.manage(type.getCode(), type, Action.UPDATE);
+    }
 
-	@Override
-	public void deleteWidgetType(String code) {
-		this.manage(code, new WidgetType(), Action.DELETE);
-	}
+    @Override
+    public void deleteWidgetType(String code) {
+        this.manage(code, new WidgetType(), Action.DELETE);
+    }
 
-	@Override
-	protected String getCodesCacheKey() {
-		return WIDGET_TYPE_CODES_CACHE_NAME;
-	}
+    @Override
+    protected String getCodesCacheKey() {
+        return WIDGET_TYPE_CODES_CACHE_NAME;
+    }
 
-	@Override
-	protected String getCacheKeyPrefix() {
-		return WIDGET_TYPE_CACHE_NAME_PREFIX;
-	}
+    @Override
+    protected String getCacheKeyPrefix() {
+        return WIDGET_TYPE_CACHE_NAME_PREFIX;
+    }
 
-	@Override
-	protected String getCacheName() {
-		return WIDGET_TYPE_MANAGER_CACHE_NAME;
-	}
+    @Override
+    protected String getCacheName() {
+        return WIDGET_TYPE_MANAGER_CACHE_NAME;
+    }
 
 }

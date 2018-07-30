b0bdae41cefac677ea502b5b4afa5e58db7fd16d
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-07-30 15:32:09 +0200

    EN-1748 test improvement

--- admin-console/src/test/java/com/agiletec/plugins/jacms/apsadmin/portal/TestPageAction.java
+++ admin-console/src/test/java/com/agiletec/plugins/jacms/apsadmin/portal/TestPageAction.java
@@ -24,61 +24,87 @@ import com.agiletec.apsadmin.ApsAdminBaseTestCase;
 import com.agiletec.apsadmin.system.ApsAdminSystemConstants;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author E.Santoboni
  */
 public class TestPageAction extends ApsAdminBaseTestCase {
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		this.init();
-	}
+    private IPageManager pageManager = null;
 
-	public void testSavePage() throws Throwable {
-		String pageCode = "customer_subpage_2";
-		IPage page = this._pageManager.getDraftPage(pageCode);
-		assertNotNull(page);
-		try {
-			PageMetadata metadata = page.getMetadata();
-			this.setUserOnSession("admin");
-			this.initAction("/do/Page", "save");
-			this.addParameter("strutsAction", String.valueOf(ApsAdminSystemConstants.EDIT));
-			this.addParameter("langit", "");
-			this.addParameter("langen", metadata.getTitle("en"));
-			this.addParameter("model", metadata.getModel().getCode());
-			this.addParameter("group", page.getGroup());
-			this.addParameter("pageCode", pageCode);
-			Collection<String> extraGroups = metadata.getExtraGroups();
-			if (null != extraGroups) {
-				Iterator<String> extraGroupIter = extraGroups.iterator();
-				while (extraGroupIter.hasNext()) {
-					String extraGroup = (String) extraGroupIter.next();
-					this.addParameter("extraGroups", extraGroup);
-				}
-			}
-			this.addParameter("extraGroups", "management");
-			String result = this.executeAction();
-			assertEquals(Action.INPUT, result);
-			ActionSupport action = this.getAction();
-			assertEquals(2, action.getFieldErrors().size());
-			assertEquals(1, action.getFieldErrors().get("langit").size());
-			assertEquals(1, action.getFieldErrors().get("extraGroups").size());
-		} catch (Throwable t) {
-			this._pageManager.updatePage(page);
-			throw t;
-		}
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.init();
+    }
 
-	private void init() throws Exception {
-		try {
-			this._pageManager = (IPageManager) this.getService(SystemConstants.PAGE_MANAGER);
-		} catch (Throwable t) {
-			throw new Exception(t);
-		}
-	}
+    public void testSavePage_1() throws Throwable {
+        String pageCode = "customer_subpage_2";
+        IPage page = this.pageManager.getDraftPage(pageCode);
+        assertNotNull(page);
+        try {
+            this.prepareTestSavePage(page, page.getGroup(), "management");
+            String result = this.executeAction();
+            assertEquals(Action.INPUT, result);
+            ActionSupport action = this.getAction();
+            assertEquals(2, action.getFieldErrors().size());
+            assertEquals(1, action.getFieldErrors().get("langit").size());
+            assertEquals(1, action.getFieldErrors().get("extraGroups").size());
+        } catch (Throwable t) {
+            this.pageManager.updatePage(page);
+            throw t;
+        }
+    }
 
-	private IPageManager _pageManager = null;
+    public void testSavePage_2() throws Throwable {
+        String pageCode = "customer_subpage_2";
+        IPage page = this.pageManager.getDraftPage(pageCode);
+        assertNotNull(page);
+        try {
+            this.prepareTestSavePage(page, "management", "");
+            String result = this.executeAction();
+            assertEquals(Action.INPUT, result);
+            ActionSupport action = this.getAction();
+            assertEquals(3, action.getFieldErrors().size());
+            assertEquals(1, action.getFieldErrors().get("group").size());
+            assertEquals(1, action.getFieldErrors().get("langit").size());
+            assertEquals(1, action.getFieldErrors().get("extraGroups").size());
+        } catch (Throwable t) {
+            this.pageManager.updatePage(page);
+            throw t;
+        }
+    }
+
+    private void prepareTestSavePage(IPage page, String mainGroup, String extendedGroup) throws Throwable {
+        PageMetadata metadata = page.getMetadata();
+        this.setUserOnSession("admin");
+        this.initAction("/do/Page", "save");
+        this.addParameter("strutsAction", String.valueOf(ApsAdminSystemConstants.EDIT));
+        this.addParameter("langit", "");
+        this.addParameter("langen", metadata.getTitle("en"));
+        this.addParameter("model", metadata.getModel().getCode());
+        this.addParameter("group", mainGroup);
+        this.addParameter("pageCode", page.getCode());
+        Collection<String> extraGroups = metadata.getExtraGroups();
+        if (null != extraGroups) {
+            Iterator<String> extraGroupIter = extraGroups.iterator();
+            while (extraGroupIter.hasNext()) {
+                String extraGroup = (String) extraGroupIter.next();
+                this.addParameter("extraGroups", extraGroup);
+            }
+        }
+        if (StringUtils.isEmpty(extendedGroup)) {
+            this.addParameter("extraGroups", extendedGroup);
+        }
+    }
+
+    private void init() throws Exception {
+        try {
+            this.pageManager = (IPageManager) this.getService(SystemConstants.PAGE_MANAGER);
+        } catch (Throwable t) {
+            throw new Exception(t);
+        }
+    }
 
 }

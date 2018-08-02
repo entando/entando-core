2a507e452e6abf5fe7b8fa8850581850b7eb07b6
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-08-02 11:20:00 +0200

    EN-1748 fix scripts for update

--- engine/src/main/db/update/scriptPort.sql
+++ engine/src/main/db/update/scriptPort.sql
@@ -1,1 +1,5 @@
-
+ALTER TABLE pages_metadata_draft ADD COLUMN groupcode character varying(30);
+ALTER TABLE pages_metadata_online ADD COLUMN groupcode character varying(30);
+UPDATE pages_metadata_online SET groupcode = (SELECT pages.groupcode FROM pages WHERE pages_metadata_online.code = pages.code);
+UPDATE pages_metadata_draft SET groupcode = (SELECT pages.groupcode FROM pages WHERE pages_metadata_draft.code = pages.code);
+ALTER TABLE pages DROP groupcode;

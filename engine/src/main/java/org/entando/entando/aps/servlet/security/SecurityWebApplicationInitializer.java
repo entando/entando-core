bddb16cdebe204a0685a877abb19addbc3e4e4c7
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-09-12 17:41:57 +0200

    EN-1937 Added Spring Security Oauth2 dependency

---
+++ engine/src/main/java/org/entando/entando/aps/servlet/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.entando.entando.aps.servlet.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}

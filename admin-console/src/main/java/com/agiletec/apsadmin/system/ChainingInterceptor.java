45f533b5a44234a9ded54e809f6f45285544dd5d
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-09-21 10:01:11 +0200

    EN-1931 Restored struts2 version upgrade

--- admin-console/src/main/java/com/agiletec/apsadmin/system/ChainingInterceptor.java
+++ admin-console/src/main/java/com/agiletec/apsadmin/system/ChainingInterceptor.java
@@ -21,31 +21,40 @@ import java.util.Set;
 /**
  * <!-- START SNIPPET: description -->
  * <p>
- * An interceptor that copies all the properties of every object in the value stack to the currently executing object,
- * except for any object that implements {@link Unchainable}. A collection of optional <i>includes</i> and
- * <i>excludes</i> may be provided to control how and which parameters are copied. Only includes or excludes may be
- * specified. Specifying both results in undefined behavior. See the javadocs for {@link ReflectionProvider#copy(Object, Object,
- * java.util.Map, java.util.Collection, java.util.Collection)} for more information.
+ * An interceptor that copies all the properties of every object in the value
+ * stack to the currently executing object, except for any object that
+ * implements {@link Unchainable}. A collection of optional <i>includes</i> and
+ * <i>excludes</i> may be provided to control how and which parameters are
+ * copied. Only includes or excludes may be specified. Specifying both results
+ * in undefined behavior. See the javadocs for {@link ReflectionProvider#copy(Object, Object,
+ * java.util.Map, java.util.Collection, java.util.Collection)} for more
+ * information.
  * </p>
  *
  * <p>
- * <b>Note:</b> It is important to remember that this interceptor does nothing if there are no objects already on the stack.
+ * <b>Note:</b> It is important to remember that this interceptor does nothing
+ * if there are no objects already on the stack.
  * <br>This means two things:
- * <br><b>One</b>, you can safely apply it to all your actions without any worry of adverse affects.
- * <br><b>Two</b>, it is up to you to ensure an object exists in the stack prior to invoking this action. The most typical way this is done
- * is through the use of the <b>chain</b> result type, which combines with this interceptor to make up the action
- * chaining feature.
+ * <br><b>One</b>, you can safely apply it to all your actions without any worry
+ * of adverse affects.
+ * <br><b>Two</b>, it is up to you to ensure an object exists in the stack prior
+ * to invoking this action. The most typical way this is done is through the use
+ * of the <b>chain</b> result type, which combines with this interceptor to make
+ * up the action chaining feature.
  * </p>
  *
  * <p>
- * <b>Note:</b> By default Errors, Field errors and Message aren't copied during chaining, to change the behaviour you can specify
- * the below three constants in struts.properties or struts.xml:
+ * <b>Note:</b> By default Errors, Field errors and Message aren't copied during
+ * chaining, to change the behaviour you can specify the below three constants
+ * in struts.properties or struts.xml:
  * </p>
  *
  * <ul>
  * <li>struts.xwork.chaining.copyErrors - set to true to copy Action Errors</li>
- * <li>struts.xwork.chaining.copyFieldErrors - set to true to copy Field Errors</li>
- * <li>struts.xwork.chaining.copyMessages - set to true to copy Action Messages</li>
+ * <li>struts.xwork.chaining.copyFieldErrors - set to true to copy Field
+ * Errors</li>
+ * <li>struts.xwork.chaining.copyMessages - set to true to copy Action
+ * Messages</li>
  * </ul>
  *
  * <p>
@@ -57,14 +66,17 @@ import java.util.Set;
  * </pre>
  *
  * <p>
- * <b>Note:</b> By default actionErrors and actionMessages are excluded when copping object's properties.
+ * <b>Note:</b> By default actionErrors and actionMessages are excluded when
+ * copping object's properties.
  * </p>
  * <!-- END SNIPPET: description -->
  * <u>Interceptor parameters:</u>
  * <!-- START SNIPPET: parameters -->
  * <ul>
- * <li>excludes (optional) - the list of parameter names to exclude from copying (all others will be included).</li>
- * <li>includes (optional) - the list of parameter names to include when copying (all others will be excluded).</li>
+ * <li>excludes (optional) - the list of parameter names to exclude from copying
+ * (all others will be included).</li>
+ * <li>includes (optional) - the list of parameter names to include when copying
+ * (all others will be excluded).</li>
  * </ul>
  * <!-- END SNIPPET: parameters -->
  * <u>Extending the interceptor:</u>
@@ -99,33 +111,33 @@ import java.util.Set;
  * @see com.opensymphony.xwork2.ActionChainResult
  */
 public class ChainingInterceptor extends com.opensymphony.xwork2.interceptor.ChainingInterceptor {
-	
+
     public void setExcludeParameters(String parametersToExclude) {
-		Set<String> parameters = TextParseUtil.commaDelimitedStringToSet(parametersToExclude);
-		if (parameters.contains("fieldErrors")) {
-			super.setCopyFieldErrors(Boolean.FALSE.toString());
-		}
-		if (parameters.contains("actionErrors")) {
-			super.setCopyErrors(Boolean.FALSE.toString());
-		}
-		if (parameters.contains("actionMessages")) {
-			super.setCopyMessages(Boolean.FALSE.toString());
-		}
-		super.setExcludes(parameters);
-	}
-	
-	public void setIncludeParameters(String parametersToInclude) {
-		Set<String> parameters = TextParseUtil.commaDelimitedStringToSet(parametersToInclude);
-		if (parameters.contains("fieldErrors")) {
-			super.setCopyFieldErrors(Boolean.TRUE.toString());
-		}
-		if (parameters.contains("actionErrors")) {
-			super.setCopyErrors(Boolean.TRUE.toString());
-		}
-		if (parameters.contains("actionMessages")) {
-			super.setCopyMessages(Boolean.TRUE.toString());
-		}
-		super.setIncludes(parameters);
-	}
-	
+        Set<String> parameters = TextParseUtil.commaDelimitedStringToSet(parametersToExclude);
+        if (parameters.contains("fieldErrors")) {
+            super.setCopyFieldErrors(Boolean.FALSE.toString());
+        }
+        if (parameters.contains("actionErrors")) {
+            super.setCopyErrors(Boolean.FALSE.toString());
+        }
+        if (parameters.contains("actionMessages")) {
+            super.setCopyMessages(Boolean.FALSE.toString());
+        }
+        super.setExcludesCollection(parameters);
+    }
+
+    public void setIncludeParameters(String parametersToInclude) {
+        Set<String> parameters = TextParseUtil.commaDelimitedStringToSet(parametersToInclude);
+        if (parameters.contains("fieldErrors")) {
+            super.setCopyFieldErrors(Boolean.TRUE.toString());
+        }
+        if (parameters.contains("actionErrors")) {
+            super.setCopyErrors(Boolean.TRUE.toString());
+        }
+        if (parameters.contains("actionMessages")) {
+            super.setCopyMessages(Boolean.TRUE.toString());
+        }
+        super.setIncludesCollection(parameters);
+    }
+
 }

d4fd3ac70405f2359705c7e90d1338be83b7c2ff
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-09-27 09:22:01 +0200

    Fixed CustomLocalizedProvider - Added option for properties into war (Jboss/Wildfly)

--- admin-console/src/main/java/org/entando/entando/apsadmin/system/resource/CustomLocalizedTextProvider.java
+++ admin-console/src/main/java/org/entando/entando/apsadmin/system/resource/CustomLocalizedTextProvider.java
@@ -51,8 +51,8 @@ public class CustomLocalizedTextProvider extends StrutsLocalizedTextProvider {
                     super.addDefaultResourceBundle(directory + RESOURCE_FILE_NAME);
                 }
             }
-        } catch (Exception t) {
-            logger.error("Error loading default resources", t);
+        } catch (Exception e) {
+            logger.error("Error loading default resources", e);
         }
     }
 
@@ -64,6 +64,13 @@ public class CustomLocalizedTextProvider extends StrutsLocalizedTextProvider {
             String urlPath = resource.getURL().getPath();
             if (urlPath.contains(LIB_FOLDER) && urlPath.contains("!") && urlPath.indexOf(LIB_FOLDER) < urlPath.indexOf("!")) {
                 int start = urlPath.indexOf("!") + 2;
+                int end = urlPath.lastIndexOf("/") + 1;
+                String directory = urlPath.substring(start, end);
+                resourceDirectories.add(directory);
+            } else if (urlPath.contains(".war" + LIB_FOLDER)
+                    && urlPath.contains(".jar") && urlPath.endsWith(".properties")
+                    && urlPath.indexOf(".war" + LIB_FOLDER) < urlPath.indexOf(".jar")) {
+                int start = urlPath.indexOf(".jar") + 5;
                 int end = urlPath.lastIndexOf("/") + 1;
                 String directory = urlPath.substring(start, end);
                 resourceDirectories.add(directory);

946028a57fbd2ada1c7dbe3c8eaab3d1f4b2a24b
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2018-10-16 18:32:41 -0300

    EN-2130 Added basic configuration for apiKey in Swagger

--- engine/src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
+++ engine/src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
@@ -1,23 +1,30 @@
 package org.entando.entando.web.swagger;
 
+import io.swagger.models.auth.In;
+import org.apache.http.HttpHeaders;
 import org.entando.entando.web.user.model.UserAuthoritiesRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 @ComponentScan
 @EnableSwagger2
 @EnableWebMvc
 public class SwaggerConfig {
+
+    private static final String API_KEY = "apiKey";
 
     @Bean
     public Docket api() {
@@ -27,113 +34,41 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
+                .securitySchemes(singletonList(apiKey()))
+                .securityContexts(singletonList(securityContext()))
                 .directModelSubstitute(UserAuthoritiesRequest.class, String.class);
     }
 
-    private ApiInfo apiInfo() {
-
-            ApiInfo apiInfo = new ApiInfo("Entando Rest API",
-                                      "Entando AppBuilder Rest APIs ",
-                                      "1",
-                                      "",
-                                      new Contact("Entando", "http://www.entando.com", null),
-                                      "",
-                                      "",
-                                          new ArrayList<>());
-        return apiInfo;
+    private ApiKey apiKey() {
+        return new ApiKey(API_KEY,
+                        HttpHeaders.AUTHORIZATION,
+                        In.HEADER.name());
     }
 
-//    @Bean
-//    public Docket petApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build()
-//                .pathMapping("/")
-//                .directModelSubstitute(LocalDate.class,
-//                                       String.class)
-//                .genericModelSubstitutes(ResponseEntity.class)
-//                .alternateTypeRules(
-//                        newRule(typeResolver.resolve(DeferredResult.class,
-//                                                     typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-//                                typeResolver.resolve(WildcardType.class)))
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(RequestMethod.GET,
-//                                       newArrayList(new ResponseMessageBuilder()
-//                                                            .code(500)
-//                                                            .message("500 message")
-//                                                            .responseModel(new ModelRef("Error"))
-//                                                            .build()))
-//                .securitySchemes(newArrayList(apiKey()))
-//                .securityContexts(newArrayList(securityContext()))
-//                .enableUrlTemplating(true)
-//                .globalOperationParameters(
-//                        newArrayList(new ParameterBuilder()
-//                                             .name("someGlobalParameter")
-//                                             .description("Description of someGlobalParameter")
-//                                             .modelRef(new ModelRef("string"))
-//                                             .parameterType("query")
-//                                             .required(true)
-//                                             .build()))
-//                .tags(new Tag("Pet Service", "All apis relating to pets"))
-//                .additionalModels(typeResolver.resolve(WidgetDto.class))
-//                ;
-//    }
-//
-//    @Autowired
-//    private TypeResolver typeResolver;
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey("mykey", "api_key", "header");
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(defaultAuth())
-//                .forPaths(PathSelectors.regex("/anyPath.*"))
-//                .build();
-//    }
-//
-//    List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope
-//                = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return newArrayList(
-//                new SecurityReference("mykey", authorizationScopes));
-//    }
-//
-//    @Bean
-//    SecurityConfiguration security() {
-//        return SecurityConfigurationBuilder.builder()
-//                .clientId("test-app-client-id")
-//                .clientSecret("test-app-client-secret")
-//                .realm("test-app-realm")
-//                .appName("test-app")
-//                .scopeSeparator(",")
-//                .additionalQueryStringParams(null)
-//                .useBasicAuthenticationWithAccessCodeGrant(false)
-//                .build();
-//    }
-//
-//    @Bean
-//    UiConfiguration uiConfig() {
-//        return UiConfigurationBuilder.builder()
-//                .deepLinking(true)
-//                .displayOperationId(false)
-//                .defaultModelsExpandDepth(1)
-//                .defaultModelExpandDepth(1)
-//                .defaultModelRendering(ModelRendering.EXAMPLE)
-//                .displayRequestDuration(false)
-//                .docExpansion(DocExpansion.NONE)
-//                .filter(false)
-//                .maxDisplayedTags(null)
-//                .operationsSorter(OperationsSorter.ALPHA)
-//                .showExtensions(false)
-//                .tagsSorter(TagsSorter.ALPHA)
-//                .validatorUrl(null)
-//                .build();
-//    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))//<17>
+                .build();
+    }
 
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(
+                new SecurityReference(API_KEY, authorizationScopes));//<18>
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Entando Rest API",
+                                  "Entando AppBuilder Rest APIs ",
+                                  "1",
+                                  "",
+                                  new Contact("Entando", "http://www.entando.com", null),
+                                  "",
+                                  "",
+                                      new ArrayList<>());
+    }
 }
--- engine/src/main/java/org/entando/entando/web/swagger/SwaggerMvcAdapter.java
+++ engine/src/main/java/org/entando/entando/web/swagger/SwaggerMvcAdapter.java
@@ -1,21 +1,11 @@
 package org.entando.entando.web.swagger;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 public class SwaggerMvcAdapter extends WebMvcConfigurerAdapter {
-
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-//        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-//        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-//        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {

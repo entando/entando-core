4f5b597cbb9f23e79da1128bcf7267615aaba4b4
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2018-10-19 08:49:26 -0300

    EN-2130 Small change to allow checking redirect url as relative

--- engine/src/main/java/org/entando/entando/aps/servlet/AuthEndpointServlet.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/AuthEndpointServlet.java
@@ -130,7 +130,7 @@ public class AuthEndpointServlet extends HttpServlet {
                     throw OAuthUtils.handleOAuthProblemException("Invalid clientId");
                 } else if (clientDetail.getExpirationDate().getTime() < System.currentTimeMillis()) {
                     throw OAuthUtils.handleOAuthProblemException("ClientId is expired");
-                } else if (!clientDetail.getCallbackUrl().equals(oauthRequest.getRedirectURI())) {
+                } else if (!oauthRequest.getRedirectURI().endsWith(clientDetail.getCallbackUrl())) {
                     throw OAuthUtils.handleOAuthProblemException("Invalid redirectUri");
                 }
 

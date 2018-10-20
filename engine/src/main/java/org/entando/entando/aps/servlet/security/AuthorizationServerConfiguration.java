2425af62a3f8c3c9d87d32ab968c77dd2b640682
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-10-20 12:16:04 +0200

    EN-1993 Created connector with OAuthConsumerManager

--- engine/src/main/java/org/entando/entando/aps/servlet/security/AuthorizationServerConfiguration.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/security/AuthorizationServerConfiguration.java
@@ -21,7 +21,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
@@ -33,7 +33,8 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     private TokenStore tokenStore;
 
     @Autowired
-    private UserApprovalHandler userApprovalHandler;
+    @Qualifier("OAuthConsumerManager")
+    private ClientDetailsService clientDetailsService;
 
     @Autowired
     @Qualifier("AuthenticationProviderManager")
@@ -41,21 +42,13 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("my-trusted-client")
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-                .scopes("read", "write", "trust")
-                .secret("secret")
-                .accessTokenValiditySeconds(120).//Access token is only valid for 2 minutes.
-                refreshTokenValiditySeconds(600);//Refresh token is only valid for 10 minutes.
-
+        clients.withClientDetails(this.clientDetailsService);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
-                .authenticationManager(authenticationManager).prefix("/api");
+        //endpoints.pathMapping("/oauth/token", "/OAuth2/access_token");
+        endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager).prefix("/api");
     }
 
 }

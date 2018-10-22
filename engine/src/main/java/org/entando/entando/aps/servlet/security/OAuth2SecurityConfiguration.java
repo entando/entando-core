7205782af404379e65cff5272bc0a6a9e932f87e
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-10-22 10:19:50 +0200

    EN-1993 Removed unused beans

--- engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
@@ -13,27 +13,19 @@
  */
 package org.entando.entando.aps.servlet.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableWebSecurity
 public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private ClientDetailsService clientDetailsService;
-
+    //@Autowired
+    //private ClientDetailsService clientDetailsService;
     // Very important for Spring 5.x
     @SuppressWarnings("deprecation")
     @Bean
@@ -49,7 +41,7 @@ public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/oauth/token").permitAll();
     }
-
+    /*
     @Bean
     @Autowired
     public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore) {
@@ -67,5 +59,5 @@ public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
         store.setTokenStore(tokenStore);
         return store;
     }
-
+     */
 }

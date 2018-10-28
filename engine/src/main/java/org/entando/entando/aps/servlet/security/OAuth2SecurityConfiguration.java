4a308e0072a5b182988a0abbeeb2fe692abb1dbf
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-10-28 18:31:25 +0100

    EN-1993 Removed plain text password

--- engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
@@ -13,22 +13,14 @@
  */
 package org.entando.entando.aps.servlet.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    @SuppressWarnings("deprecation")
-    @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {

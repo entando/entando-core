25b776eb7bac7353b8f9a21b9d4c082326f5183f
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2018-12-26 10:56:58 -0200

    EN-2299 Added a path provider to change path resolution with reverse proxies

--- engine/src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
+++ engine/src/main/java/org/entando/entando/web/swagger/SwaggerConfig.java
@@ -3,39 +3,52 @@ package org.entando.entando.web.swagger;
 import com.agiletec.aps.system.SystemConstants;
 import com.agiletec.aps.system.services.baseconfig.BaseConfigManager;
 import com.fasterxml.classmate.TypeResolver;
-import java.lang.reflect.WildcardType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.Ordered;
-import springfox.documentation.schema.AlternateTypeRules;
 import org.entando.entando.web.user.model.UserAuthoritiesRequest;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.Ordered;
+import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.*;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Map;
-import java.util.List;
+import javax.servlet.ServletContext;
+import java.lang.reflect.WildcardType;
+import java.net.*;
+import java.util.*;
 
 import static java.util.Collections.singletonList;
 
 @ComponentScan
 @EnableSwagger2
 @EnableWebMvc
-public class SwaggerConfig {
+public class SwaggerConfig implements ServletContextAware {
 
     private static final String ENTANDO_AUTH = "entando_auth";
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private ServletContext servletContext;
+
+    private final TypeResolver typeResolver;
+    private final BaseConfigManager baseConfigManager;
+
     @Autowired
-    private TypeResolver typeResolver;
-
-    private BaseConfigManager baseConfigManager;
-
-    public SwaggerConfig(BaseConfigManager baseConfigManager) {
+    public SwaggerConfig(TypeResolver typeResolver, BaseConfigManager baseConfigManager) {
+        this.typeResolver = typeResolver;
         this.baseConfigManager = baseConfigManager;
+    }
+
+    @Override
+    public void setServletContext(ServletContext context) {
+        this.servletContext = context;
     }
 
     @Bean
@@ -45,6 +58,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .pathProvider(relativePathProvider())
                 .apiInfo(apiInfo())
                 .securitySchemes(singletonList(oAuth()))
                 .securityContexts(singletonList(securityContext()))
@@ -52,9 +66,39 @@ public class SwaggerConfig {
                 .alternateTypeRules(
                         // This rule is necessary to allow Swagger resolving Map<String, List<String>> types
                         AlternateTypeRules.newRule(
-                                typeResolver.resolve(Map.class, String.class, typeResolver.resolve(List.class, String.class)),
-                                typeResolver.resolve(Map.class, String.class, WildcardType.class), Ordered.HIGHEST_PRECEDENCE)
+                                typeResolver.resolve(
+                                        Map.class,
+                                        String.class,
+                                        typeResolver.resolve(List.class, String.class)),
+                                typeResolver.resolve(
+                                        Map.class,
+                                        String.class,
+                                        WildcardType.class),
+                                Ordered.HIGHEST_PRECEDENCE)
                         );
+    }
+
+    private String getBaseConfigUrl() {
+        String baseUrl = "";
+
+        try {
+            URL configUrl = new URL(baseConfigManager.getParam(SystemConstants.PAR_APPL_BASE_URL));
+            baseUrl = configUrl.getPath();
+
+        } catch (MalformedURLException e) {
+            logger.warn("Couldn't configure Swagger baseUrl");
+        }
+
+        return baseUrl;
+    }
+
+    private RelativePathProvider relativePathProvider() {
+        return new RelativePathProvider(servletContext) {
+            @Override
+            public String getApplicationBasePath(){
+                return getBaseConfigUrl();
+            }
+        };
     }
 
     private OAuth oAuth() {
@@ -68,7 +112,8 @@ public class SwaggerConfig {
     private List<GrantType> grantTypes() {
         String authUrl = baseConfigManager.getParam(SystemConstants.PAR_APPL_BASE_URL);
 
-        return singletonList(new ResourceOwnerPasswordCredentialsGrant(authUrl + "api/oauth/token"));
+        return singletonList(new ResourceOwnerPasswordCredentialsGrant(
+                authUrl + "api/oauth/token"));
     }
 
     private AuthorizationScope globalScope() {

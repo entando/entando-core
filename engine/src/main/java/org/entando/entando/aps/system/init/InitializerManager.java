1184c1f4c336d92a3ae5d8a0ce4260e617b68782
Sonia Zorba <zonia3000@gmail.com>
2019-01-17 17:54:34 +0100

    EN-2312 Changes for supporting installation of a single component (#764)
    * EN-2312 Made public some methods in DatabaseManager
    
    * EN-2312 Changes for supporting DE component installation
    
    * EN-2312 Small changes on executePostInitProcesses() method

--- engine/src/main/java/org/entando/entando/aps/system/init/InitializerManager.java
+++ engine/src/main/java/org/entando/entando/aps/system/init/InitializerManager.java
@@ -37,164 +37,164 @@ import com.agiletec.aps.system.exception.ApsSystemException;
  */
 public class InitializerManager extends AbstractInitializerManager implements IInitializerManager {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	public static final String REPORT_CONFIG_ITEM = "entandoComponentsReport";
+    public static final String REPORT_CONFIG_ITEM = "entandoComponentsReport";
 
-	private IInitializerManagerCacheWrapper cacheWrapper;
+    private IInitializerManagerCacheWrapper cacheWrapper;
 
-	private boolean checkOnStartup;
+    private boolean checkOnStartup;
 
-	private Map<String, IPostProcessor> postProcessors;
+    private Map<String, IPostProcessor> postProcessors;
 
-	private IDatabaseManager databaseManager;
+    private IDatabaseManager databaseManager;
 
-	@Override
-	public SystemInstallationReport getCurrentReport() {
-		return this.getCacheWrapper().getReport();
-	}
+    @Override
+    public SystemInstallationReport getCurrentReport() {
+        return this.getCacheWrapper().getReport();
+    }
 
-	protected IInitializerManagerCacheWrapper getCacheWrapper() {
-		return cacheWrapper;
-	}
+    protected IInitializerManagerCacheWrapper getCacheWrapper() {
+        return cacheWrapper;
+    }
 
-	public void setCacheWrapper(IInitializerManagerCacheWrapper cacheWrapper) {
-		this.cacheWrapper = cacheWrapper;
-	}
+    public void setCacheWrapper(IInitializerManagerCacheWrapper cacheWrapper) {
+        this.cacheWrapper = cacheWrapper;
+    }
 
-	protected boolean isCheckOnStartup() {
-		return checkOnStartup;
-	}
+    protected boolean isCheckOnStartup() {
+        return checkOnStartup;
+    }
 
-	public void setCheckOnStartup(boolean checkOnStartup) {
-		this.checkOnStartup = checkOnStartup;
-	}
+    public void setCheckOnStartup(boolean checkOnStartup) {
+        this.checkOnStartup = checkOnStartup;
+    }
 
-	public Map<String, IPostProcessor> getPostProcessors() {
-		return postProcessors;
-	}
+    public Map<String, IPostProcessor> getPostProcessors() {
+        return postProcessors;
+    }
 
-	public void setPostProcessors(Map<String, IPostProcessor> postProcessors) {
-		this.postProcessors = postProcessors;
-	}
+    public void setPostProcessors(Map<String, IPostProcessor> postProcessors) {
+        this.postProcessors = postProcessors;
+    }
 
-	protected IDatabaseManager getDatabaseManager() {
-		return databaseManager;
-	}
+    protected IDatabaseManager getDatabaseManager() {
+        return databaseManager;
+    }
 
-	public void setDatabaseManager(IDatabaseManager databaseManager) {
-		this.databaseManager = databaseManager;
-	}
+    public void setDatabaseManager(IDatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
 
+    public void init() throws Exception {
+        SystemInstallationReport report = null;
+        try {
+            report = this.extractReport();
+            report = ((IDatabaseInstallerManager) this.getDatabaseManager()).installDatabase(report, this.isCheckOnStartup());
+            this.getCacheWrapper().initCache(report);
+        } catch (Throwable t) {
+            logger.error("Error while initializating Db Installer", t);
+            throw new Exception("Error while initializating Db Installer", t);
+        } finally {
+            if (null != report && report.isUpdated()) {
+                this.saveReport(report);
+            }
+        }
+        logger.debug("{}: initializated - Check on startup {}", this.getClass().getName(), this.isCheckOnStartup());
+    }
 
-	public void init() throws Exception {
-		SystemInstallationReport report = null;
-		try {
-			report = this.extractReport();
-			report = ((IDatabaseInstallerManager) this.getDatabaseManager()).installDatabase(report, this.isCheckOnStartup());
-			this.getCacheWrapper().initCache(report);
-		} catch (Throwable t) {
-			logger.error("Error while initializating Db Installer", t);
-			throw new Exception("Error while initializating Db Installer", t);
-		} finally {
-			if (null != report && report.isUpdated()) {
-				this.saveReport(report);
-			}
-		}
-		logger.debug("{}: initializated - Check on startup {}", this.getClass().getName(), this.isCheckOnStartup());
-	}
+    public void executePostInitProcesses() throws BeansException {
+        SystemInstallationReport report = null;
+        try {
+            report = this.extractReport();
+            List<Component> components = this.getComponentManager().getCurrentComponents();
+            for (Component component : components) {
+                executeComponentPostInitProcesses(component, report);
+            }
+        } catch (Throwable t) {
+            logger.error("Error while executing post processes", t);
+            throw new FatalBeanException("Error while executing post processes", t);
+        } finally {
+            if (null != report && report.isUpdated()) {
+                this.saveReport(report);
+            }
+        }
+    }
 
-	public void executePostInitProcesses() throws BeansException {
-		SystemInstallationReport report = null;
-		try {
-			report = this.extractReport();
-			List<Component> components = this.getComponentManager().getCurrentComponents();
-			for (int i = 0; i < components.size(); i++) {
-				Component component = components.get(i);
-				ComponentInstallationReport componentReport = report.getComponentReport(component.getCode(), false);
-				SystemInstallationReport.Status postProcessStatus = componentReport.getPostProcessStatus();
-				if (!postProcessStatus.equals(SystemInstallationReport.Status.INIT)) {
-					continue;
-				}
-				String compEnvKey = (AbstractInitializerManager.Environment.test.equals(this.getEnvironment())) 
-						? AbstractInitializerManager.Environment.test.toString() : AbstractInitializerManager.Environment.production.toString();
-				ComponentEnvironment componentEnvironment = (null != component.getEnvironments()) ? component.getEnvironments().get(compEnvKey) : null;
-				List<IPostProcess> postProcesses = (null != componentEnvironment) ? componentEnvironment.getPostProcesses() : null;
-				if (null == postProcesses || postProcesses.isEmpty()) {
-					postProcessStatus = SystemInstallationReport.Status.NOT_AVAILABLE;
-				} else if (!this.isCheckOnStartup()) {
-					postProcessStatus = SystemInstallationReport.Status.SKIPPED;
-				} else if (!componentReport.isPostProcessExecutionRequired()) {
-					//Porting or restore
-					postProcessStatus = SystemInstallationReport.Status.NOT_AVAILABLE;
-				} else {
-					postProcessStatus = this.executePostProcesses(postProcesses);
-				}
-				componentReport.setPostProcessStatus(postProcessStatus);
-				report.setUpdated();
-			}
-		} catch (Throwable t) {
-			logger.error("Error while executing post processes", t);
-			throw new FatalBeanException("Error while executing post processes", t);
-		} finally {
-			if (null != report && report.isUpdated()) {
-				this.saveReport(report);
-			}
-		}
-	}
+    public void executeComponentPostInitProcesses(Component component, SystemInstallationReport report) throws ApsSystemException {
+        ComponentInstallationReport componentReport = report.getComponentReport(component.getCode(), false);
+        SystemInstallationReport.Status postProcessStatus = componentReport.getPostProcessStatus();
+        if (!postProcessStatus.equals(SystemInstallationReport.Status.INIT)) {
+            return;
+        }
+        String compEnvKey = (AbstractInitializerManager.Environment.test.equals(this.getEnvironment()))
+                ? AbstractInitializerManager.Environment.test.toString() : AbstractInitializerManager.Environment.production.toString();
+        ComponentEnvironment componentEnvironment = (null != component.getEnvironments()) ? component.getEnvironments().get(compEnvKey) : null;
+        List<IPostProcess> postProcesses = (null != componentEnvironment) ? componentEnvironment.getPostProcesses() : null;
+        if (null == postProcesses || postProcesses.isEmpty()) {
+            postProcessStatus = SystemInstallationReport.Status.NOT_AVAILABLE;
+        } else if (!this.isCheckOnStartup()) {
+            postProcessStatus = SystemInstallationReport.Status.SKIPPED;
+        } else if (!componentReport.isPostProcessExecutionRequired()) {
+            //Porting or restore
+            postProcessStatus = SystemInstallationReport.Status.NOT_AVAILABLE;
+        } else {
+            postProcessStatus = this.executePostProcesses(postProcesses);
+        }
+        componentReport.setPostProcessStatus(postProcessStatus);
+        report.setUpdated();
+    }
 
-	protected SystemInstallationReport.Status executePostProcesses(List<IPostProcess> postProcesses) throws ApsSystemException {
-		if (null == postProcesses || postProcesses.isEmpty()) {
-			return SystemInstallationReport.Status.NOT_AVAILABLE;
-		}
-		for (int i = 0; i < postProcesses.size(); i++) {
-			IPostProcess postProcess = postProcesses.get(i);
-			try {
-				IPostProcessor postProcessor = this.getPostProcessors().get(postProcess.getCode());
-				if (null != postProcessor) {
-					postProcessor.executePostProcess(postProcess);
-				} else {
-					logger.error("Missing Post Processor for process '{}'", postProcess.getCode());
-				}
-			} catch (InvalidPostProcessResultException t) {
-				logger.error("Error while executing post process of index {}", i, t);
-				return SystemInstallationReport.Status.INCOMPLETE;
-			} catch (Throwable t) {
-				logger.error("Error while executing post process - index {}", i, t);
-				return SystemInstallationReport.Status.INCOMPLETE;
-			}
-		}
-		return SystemInstallationReport.Status.OK;
-	}
+    protected SystemInstallationReport.Status executePostProcesses(List<IPostProcess> postProcesses) throws ApsSystemException {
+        if (null == postProcesses || postProcesses.isEmpty()) {
+            return SystemInstallationReport.Status.NOT_AVAILABLE;
+        }
+        for (int i = 0; i < postProcesses.size(); i++) {
+            IPostProcess postProcess = postProcesses.get(i);
+            try {
+                IPostProcessor postProcessor = this.getPostProcessors().get(postProcess.getCode());
+                if (null != postProcessor) {
+                    postProcessor.executePostProcess(postProcess);
+                } else {
+                    logger.error("Missing Post Processor for process '{}'", postProcess.getCode());
+                }
+            } catch (InvalidPostProcessResultException t) {
+                logger.error("Error while executing post process of index {}", i, t);
+                return SystemInstallationReport.Status.INCOMPLETE;
+            } catch (Throwable t) {
+                logger.error("Error while executing post process - index {}", i, t);
+                return SystemInstallationReport.Status.INCOMPLETE;
+            }
+        }
+        return SystemInstallationReport.Status.OK;
+    }
 
-	@Override
-	public void reloadCurrentReport() {
-		try {
-			SystemInstallationReport report = this.extractReport();
-			this.getCacheWrapper().setCurrentReport(report);
-		} catch (Throwable t) {
-			logger.error("Error reloading report", t);
-			throw new RuntimeException("Error reloading report", t);
-		}
-	}
+    @Override
+    public void reloadCurrentReport() {
+        try {
+            SystemInstallationReport report = this.extractReport();
+            this.getCacheWrapper().setCurrentReport(report);
+        } catch (Throwable t) {
+            logger.error("Error reloading report", t);
+            throw new RuntimeException("Error reloading report", t);
+        }
+    }
 
-	//-------------------- REPORT -------- START
-
-	private void saveReport(SystemInstallationReport report) throws BeansException {
-		if (null == report || report.getReports().isEmpty()) {
-			return;
-		}
-		try {
-			InstallationReportDAO dao = new InstallationReportDAO();
-			DataSource dataSource = (DataSource) this.getBeanFactory().getBean("portDataSource");
-			dao.setDataSource(dataSource);
-			dao.saveConfigItem(report.toXml(), this.getConfigVersion());
-			this.getCacheWrapper().setCurrentReport(report);
-		} catch (Throwable t) {
-			logger.error("Error saving report", t);
-			throw new FatalBeanException("Error saving report", t);
-		}
-	}
+    //-------------------- REPORT -------- START
+    public void saveReport(SystemInstallationReport report) throws BeansException {
+        if (null == report || report.getReports().isEmpty()) {
+            return;
+        }
+        try {
+            InstallationReportDAO dao = new InstallationReportDAO();
+            DataSource dataSource = (DataSource) this.getBeanFactory().getBean("portDataSource");
+            dao.setDataSource(dataSource);
+            dao.saveConfigItem(report.toXml(), this.getConfigVersion());
+            this.getCacheWrapper().setCurrentReport(report);
+        } catch (Throwable t) {
+            logger.error("Error saving report", t);
+            throw new FatalBeanException("Error saving report", t);
+        }
+    }
 
 }
-

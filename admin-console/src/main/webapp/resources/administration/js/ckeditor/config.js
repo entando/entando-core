5c325f0248f021454eba62b6800b2e5bd06087e8
Luca Cherubin <luca.cherubin@gmail.com>
2019-07-04 13:39:37 +0200

    Fix owasp vulnerabilities (#811)
    * Solves main dependencies vulnerabilities except hibernate validator
    
    * Updates ckeditor version to 4.12.0 for owasp vulnerability issues
    
    * Updates bootstrap to version 3.4.1
    
    * Fixes vulnerabilities for hibernate-validator
    
    * Removes javassist library from ognl for conflicts with digital-exchage plugin and hibernate-entitymanager own version of the javassist library
    
    * Updates jquery version to 3.4.1 and jQuery UI to 1.12
    
    * Adds required dependencies
    
    * Changes formats of the dependency-report to ALL

--- admin-console/src/main/webapp/resources/administration/js/ckeditor/config.js
+++ admin-console/src/main/webapp/resources/administration/js/ckeditor/config.js
@@ -1,15 +1,14 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },

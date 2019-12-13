cf0284e8261beb743d40e617ac9c67c0776303f4
Kerruba <luca.cherubin@gmail.com>
2019-12-13 18:48:23 +0100

    Fix the xss issue

--- portal-ui/src/main/java/org/entando/entando/aps/servlet/XSSRequestWrapper.java
+++ portal-ui/src/main/java/org/entando/entando/aps/servlet/XSSRequestWrapper.java
@@ -13,64 +13,70 @@
  */
 package org.entando.entando.aps.servlet;
 
-import java.util.regex.Pattern;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.errors.ValidationException;
 
 public class XSSRequestWrapper extends HttpServletRequestWrapper {
 
-	private static Pattern[] patterns = new Pattern[]{
-		Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE),
-		Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-		Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-		Pattern.compile("</script>", Pattern.CASE_INSENSITIVE),
-		Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-		Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-		Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
-		Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
-		Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
-		Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
-	};
+    private static final Logger LOGGER = Logger.getLogger(XSSRequestWrapper.class.getName());
 
-	public XSSRequestWrapper(HttpServletRequest servletRequest) {
-		super(servletRequest);
-	}
+    public XSSRequestWrapper(HttpServletRequest servletRequest) {
+            super(servletRequest);
+    }
 
-	@Override
-	public String[] getParameterValues(String parameter) {
-		String[] values = super.getParameterValues(parameter);
-		if (values == null) {
-			return null;
-		}
-		int count = values.length;
-		String[] encodedValues = new String[count];
-		for (int i = 0; i < count; i++) {
-			encodedValues[i] = this.stripXSS(values[i]);
-		}
-		return encodedValues;
-	}
+    @Override
+    public String[] getParameterValues(String parameter) {
+            String[] values = super.getParameterValues(parameter);
+            if (values == null) {
+                    return null;
+            }
+            int count = values.length;
+            String[] encodedValues = new String[count];
+            for (int i = 0; i < count; i++) {
+                    encodedValues[i] = validatedParameter(values[i]);
+            }
+            return encodedValues;
+    }
 
-	@Override
-	public String getParameter(String parameter) {
-		String value = super.getParameter(parameter);
-		return this.stripXSS(value);
-	}
+    @Override
+    public String getParameter(String parameter) {
+        return validatedParameter(parameter);
+    }
 
-	@Override
-	public String getHeader(String name) {
-		String value = super.getHeader(name);
-		return this.stripXSS(value);
-	}
+    @Override
+    public String getHeader(String name) {
+            return validatedHeader(name);
+    }
 
-	private String stripXSS(String value) {
-		if (value != null) {
-			value = ESAPI.encoder().canonicalize(value);
-			value = value.replaceAll("\0", "");
-			for (Pattern scriptPattern : patterns) {
-				value = scriptPattern.matcher(value).replaceAll("");
-			}
-		}
-		return value;
-	}
+    private String validatedParameter(String name) {
+            String value = super.getParameter(name);
+            try {
+                    value = ESAPI.validator().getValidInput("HTTP parameter value: " + value, value, "HTTPParameterValue", 2000, true);
+            } catch (ValidationException e) {
+                    LOGGER.log(Level.SEVERE,
+                                    String.format("Invalid parameter (%s - %s), encoding as HTML attribute", name, value),
+                                    e);
+                    value = ESAPI.encoder().encodeForHTMLAttribute(value);
+            }
+            return value;
+    }
+
+    private String validatedHeader(String name) {
+            String value = super.getHeader(name);
+            try {
+                    value = ESAPI.validator().getValidInput("HTTP header value: " + value, value, "HTTPHeaderValue", 150, false);
+            } catch (ValidationException e) {
+                    LOGGER.log(Level.SEVERE,
+                                    String.format("Invalid header (%s - %s), encoding as HTML attribute", name, value),
+                                    e);
+                    value = ESAPI.encoder().encodeForHTMLAttribute(value);
+            }
+            return value;
+    }
+
 }
+

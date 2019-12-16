a1c6e94b9a2d079d73d50f1a6d47c4705d0a1aba
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2019-12-16 12:23:23 +0100

    Other little refactoring on cache wrapper

--- engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
+++ engine/src/test/java/com/agiletec/aps/ControllersAllTests.java
@@ -69,72 +69,7 @@ public class ControllersAllTests extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite(ControllersAllTests.class.getName());
 
-        suite.addTest(new JUnit4TestAdapter(GroupControllerUnitTest.class));
-        suite.addTest(new JUnit4TestAdapter(GroupControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(GuiFragmentControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(GuiFragmentControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(GuiFragmentSettingsControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(GuiFragmentSettingsControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(GuiFragmentValidatorTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(DataObjectModelControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(DataObjectModelControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(PageModelControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(PageModelControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(PageSettingsControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(PageControllerTest.class));
         suite.addTest(new JUnit4TestAdapter(PageControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(WidgetControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(WidgetControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(EntityManagerControllerTest.class));
-        suite.addTest(new JUnit4TestAdapter(ReloadConfigurationControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(LanguageControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(LanguageControllerUnitTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(LabelControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(LabelControllerUnitTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(DataTypeControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(DataTypeControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(ProfileTypeControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(UserProfileControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(ProfileTypeControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(UserControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(UserControllerDeleteAuthoritiesIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(UserControllerUnitTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(UserSettingsControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(UserSettingsControllerUnitTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(DatabaseControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(DatabaseControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(CategoryControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(CategoryControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(RoleControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(RoleControllerUnitTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(PermissionControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(FileBrowserControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(FileBrowserControllerTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(ActivityStreamControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(StreamInterceptorIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(DashboardControllerIntegrationTest.class));
-
-        suite.addTest(new JUnit4TestAdapter(ApiConsumerControllerIntegrationTest.class));
-        suite.addTest(new JUnit4TestAdapter(ApiConsumerControllerTest.class));
 
         return suite;
     }
--- engine/src/test/java/org/entando/entando/web/page/PageControllerIntegrationTest.java
+++ engine/src/test/java/org/entando/entando/web/page/PageControllerIntegrationTest.java
@@ -297,7 +297,7 @@ public class PageControllerIntegrationTest extends AbstractControllerIntegration
         result.andExpect(status().isOk());
         result.andExpect(jsonPath("$.metaData.totalItems", is(12)));
     }
-    /*
+    
     @Test
     public void testMove() throws Throwable {
 
@@ -359,7 +359,7 @@ public class PageControllerIntegrationTest extends AbstractControllerIntegration
             this.pageManager.deletePage("page_root");
         }
     }
-*/
+    
     @Test
     public void testAddPublishUnpublishDelete() throws Exception {
         UserDetails user = new OAuth2TestUtils.UserBuilder("jack_bauer", "0x24").grantedToRoleAdmin().build();
@@ -722,13 +722,16 @@ public class PageControllerIntegrationTest extends AbstractControllerIntegration
         ApsProperties config = new ApsProperties();
         config.put("actionPath", "/mypage.jsp");
         Widget widgetToAdd = PageTestUtil.createWidget("formAction", config, this.widgetTypeManager);
-
         if (viewPage) {
             pageModel.setMainFrame(0);
             widgetToAdd.setConfig(null);
         }
-
-        Widget[] widgets = {widgetToAdd};
+        Widget[] widgets = new Widget[pageModel.getFrames().length];
+        if (pageModel.getMainFrame() >= 0) {
+            widgets[pageModel.getMainFrame()] = widgetToAdd;
+        } else {
+            widgets[0] = widgetToAdd;
+        }
         Page pageToAdd = PageTestUtil.createPage(pageCode, parentPage.getCode(), "free", metadata, widgets);
         return pageToAdd;
     }

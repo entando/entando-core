62fc04e49f82c977d4665208690ca557a75a7748
Alessio Pintus <alessio.pintus@gmail.com>
2020-05-29 16:48:55 +0200

    ENG-462: Added a validation to avoid user to delete himself

--- engine/src/test/java/org/entando/entando/web/user/validator/UserValidatorTest.java
+++ engine/src/test/java/org/entando/entando/web/user/validator/UserValidatorTest.java
@@ -1,22 +1,26 @@
 package org.entando.entando.web.user.validator;
 
-import org.junit.Test;
-import org.springframework.validation.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.List;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.ObjectError;
 
 public class UserValidatorTest {
 
     @Test
     public void deleteAdminIsNotValid() {
-        assertFalse(UserValidator.isValidDeleteUser("admin"));
+        assertTrue(UserValidator.isAdminUser("admin"));
     }
 
     @Test
     public void deleteNotAdminIsValid() {
-        assertTrue(UserValidator.isValidDeleteUser("notAdmin"));
+        assertFalse(UserValidator.isAdminUser("notAdmin"));
     }
 
     @Test
@@ -30,5 +34,20 @@ public class UserValidatorTest {
 
         assertEquals(UserValidator.ERRCODE_DELETE_ADMIN, adminError.getCode());
         assertEquals("user.admin.cant.delete", adminError.getDefaultMessage());
+    }
+
+    @Test
+    public void createErrorSelfDeleteContainsErrorMessage() {
+        MapBindingResult bindingResult = new MapBindingResult(new HashMap<Object, Object>(), "username");
+
+        BindingResult bindingSelfDeleteError = UserValidator.createSelfDeleteUserError(bindingResult);
+
+        assertEquals(1, bindingSelfDeleteError.getErrorCount());
+
+        List<ObjectError> errors = bindingSelfDeleteError.getAllErrors();
+        ObjectError error = errors.get(0);
+
+        assertEquals(UserValidator.ERRCODE_SELF_DELETE, error.getCode());
+        assertEquals("user.self.delete.error", error.getDefaultMessage());
     }
 }

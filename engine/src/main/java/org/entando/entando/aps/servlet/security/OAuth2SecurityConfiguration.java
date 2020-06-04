c9e73bebd8ef1d4b5172aa927eff920ac6334f5e
Breno Queiroz <b.queiroz@entando.com>
2020-06-04 00:49:41 -0300

    ENG-442 Removing unecessary import

--- engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
+++ engine/src/main/java/org/entando/entando/aps/servlet/security/OAuth2SecurityConfiguration.java
@@ -19,7 +19,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
